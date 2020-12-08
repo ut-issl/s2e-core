eb12e54bb56dd3ed35c1ef3e303ffe5619fa38ab
Satoshi Ikari <ikari@space.t.u-tokyo.ac.jp>
2020-12-08 01:07:58 +0000

    Merge branch 'feature/take_structure_ini_out_from_sat_ini' into 'develop'
    Take out Structure info from Sat.ini
    
    See merge request ut_issl/s2e/s2e_core_oss!90
    
    Co-authored-by: Toshihiro Suzuki <suzuki.toshihiro.97@gmail.com>

--- src/Simulation/Spacecraft/Structure/Structure.cpp
+++ src/Simulation/Spacecraft/Structure/Structure.cpp
@@ -14,7 +14,13 @@ Structure::~Structure()
 
 void Structure::Initialize(SimulationConfig* sim_config, const int sat_id)
 {
-  kinnematics_params_ = new KinematicsParams(InitKinematicsParams(sim_config->sat_file_[sat_id]));
-  surfaces_ = InitSurfaces(sim_config->sat_file_[sat_id]);
-  rmm_params_ = new RMMParams(InitRMMParams(sim_config->sat_file_[sat_id]));
+  // Read file name
+  IniAccess conf = IniAccess(sim_config->sat_file_[sat_id]);
+  string ini_fname = conf.ReadString("STRUCTURE_FILE", "structure_file");
+  // Save ini file
+  sim_config->main_logger_->CopyFileToLogDir(ini_fname);
+  // Initialize
+  kinnematics_params_ = new KinematicsParams(InitKinematicsParams(ini_fname));
+  surfaces_ = InitSurfaces(ini_fname);
+  rmm_params_ = new RMMParams(InitRMMParams(ini_fname));
 }
