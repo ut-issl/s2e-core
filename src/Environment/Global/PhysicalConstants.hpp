73bea9c429df1e3586403b0af7cab96dbfdbb410
Satoshi Ikari <ikari@space.t.u-tokyo.ac.jp>
2022-04-05 17:09:08 +0200

    fix small

--- src/Environment/Global/PhysicalConstants.hpp
+++ src/Environment/Global/PhysicalConstants.hpp
@@ -31,7 +31,7 @@ DEFINE_PHYSICAL_CONSTANT(astronomical_unit_m, 1.49597870700e11L);               
 DEFINE_PHYSICAL_CONSTANT(gravitational_constant_Nm2_kg2, 6.67428e-11L);         /* best estimates Nm2/kg2 */
 DEFINE_PHYSICAL_CONSTANT(earth_equatorial_radius_m, 6378136.6L);                /* best estimates m */
 DEFINE_PHYSICAL_CONSTANT(earth_polar_radius_m, 6378136.6L);                     /* best estimates m */
-DEFINE_PHYSICAL_CONSTANT(earth_gravitational_constant_m3_s2, 3.986004415e14 L); /* best estimates, TT m3/s2 */
+DEFINE_PHYSICAL_CONSTANT(earth_gravitational_constant_m3_s2, 3.986004415e14L);  /* best estimates, TT m3/s2 */
 
 
 #undef DEFINE_PHYSICAL_CONSTANT
