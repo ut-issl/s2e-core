7b54734c36b47f9ff32781df7c04ead2954b2240
Satoshi Ikari <ikari@space.t.u-tokyo.ac.jp>
2022-04-05 19:43:04 +0200

    add RAD2DEG constant

--- src/Library/math/Constant.hpp
+++ src/Library/math/Constant.hpp
@@ -37,6 +37,7 @@ static_assert(3.14 < pi && pi < 3.15, "pi");
 
 // angle conversion
 DEFINE_MATH_CONSTANT(deg_to_rad, pi/180.0L);              /* degree to radian */
+DEFINE_MATH_CONSTANT(rad_to_deg, 180.0L/pi);              /* radian to degree */
 DEFINE_MATH_CONSTANT(arcsec_to_rad, deg_to_rad/3600.0L);  /* arcsecond to radian */
 
 #undef DEFINE_MATH_CONSTANT
