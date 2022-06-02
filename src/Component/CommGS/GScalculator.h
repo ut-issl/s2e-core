606f945db2e21fe1650039fd099fe7a19b7662e0
Satoshi Ikari <ikari@space.t.u-tokyo.ac.jp>
2022-06-02 13:44:17 +0200

    Refactor GScalculation

--- src/Component/CommGS/GScalculator.h
+++ src/Component/CommGS/GScalculator.h
@@ -1,8 +1,7 @@
 /*
  * @file GScalculator.h
- * @brief 地上局クラスが持つ，通信関連計算模擬クラスです．
+ * @brief Emuration of analysis and calculation for Ground Stations
  * @author 山本 智貴
- * @date 2020.05.26
  */
 
 #pragma once
@@ -21,6 +20,20 @@ using libra::Vector;
 
 class GScalculator : public ILoggable {
  public:
+  GScalculator(const double loss_polarization, const double loss_atmosphere, const double loss_rainfall, const double loss_others, const double EbN0,
+               const double hardware_deterioration, const double coding_gain, const double margin_req);
+  virtual ~GScalculator();
+  void Update(const Dynamics& dynamics, const Antenna& sc_ant, const GroundStation& groundstation, const Antenna& gs_ant);
+
+  // ILoggable
+  virtual std::string GetLogHeader() const;
+  virtual std::string GetLogValue() const;
+
+  // Getter
+  inline bool IsVisible() const { return is_visible_; }
+  inline bool GetMaxBitrate() const { return max_bitrate_; }
+
+ protected:
   double loss_polarization_;       //[dB]
   double loss_atmosphere_;         //[dB]
   double loss_rainfall_;           //[dB]
@@ -30,22 +43,12 @@ class GScalculator : public ILoggable {
   double coding_gain_;             //[dB]
   double margin_req_;              //[dB]
 
-  bool visible_flag_;
+  bool is_visible_;
   double max_bitrate_;  //[kbps]
 
-  GScalculator(double loss_polarization, double loss_atmosphere, double loss_rainfall, double loss_others, double EbN0, double hardware_deterioration,
-               double coding_gain, double margin_req);
-  virtual ~GScalculator();
-  void Initialize();
-  void Update(const Dynamics& dynamics, const Antenna& sc_ant, const GroundStation& groundstation, const Antenna& gs_ant);
+  // Return true when the satellite is visible from the ground station
+  bool CalcIsVisible(const Dynamics& dynamics, const GroundStation& groundstation);
 
-  virtual std::string GetLogHeader() const;
-  virtual std::string GetLogValue() const;
-
- protected:
-  // 衛星と地上局が可視の位置関係のときにtrue，非可視のときにfalseを返す
-  bool IsVisible(const Dynamics& dynamics, const GroundStation& groundstation);
-
-  // 最大可能ビットレートを回線計算をもとに計算する
+  // Calculate the maximum bitrate
   double CalcMaxBitrate(const Dynamics& dynamics, const Antenna& sc_ant, const GroundStation& groundstation, const Antenna& gs_ant);
 };
