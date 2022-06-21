3269cc9eedcfe546fc34f0803f4775c7a80b05cc
Satoshi Ikari <ikari@space.t.u-tokyo.ac.jp>
2022-06-21 15:20:28 +0200

    Delete Japanese comment

--- src/Disturbance/GravityGradient.cpp
+++ src/Disturbance/GravityGradient.cpp
@@ -9,10 +9,9 @@
 
 using namespace std;
 
-GravityGradient::GravityGradient() : GravityGradient(environment::earth_gravitational_constant_m3_s2) {  //デフォルトコンストラクタ
-}
+GravityGradient::GravityGradient() : GravityGradient(environment::earth_gravitational_constant_m3_s2) {}
 
-GravityGradient::GravityGradient(const double mu_e_input) {  //コンストラクタ
+GravityGradient::GravityGradient(const double mu_e_input) {
   fill_up(torque_b_, 0.0);
   mu_e_ = mu_e_input;
 }
@@ -21,15 +20,12 @@ void GravityGradient::Update(const LocalEnvironment& local_env, const Dynamics& 
   CalcTorque(local_env.GetCelesInfo().GetPosFromSC_b("EARTH"), dynamics.GetAttitude().GetInertiaTensor());
 }
 
-Vector<3> GravityGradient::CalcTorque(
-    double R0, Vector<3> u_b,
-    Matrix<3, 3> I_b) {  //トルクを取得、引数は前から地球半径、地球中心方向単位ベクトル、慣性テンソル。引数の単位はそれぞれm,無次元,kg*m^2
+Vector<3> GravityGradient::CalcTorque(double R0, Vector<3> u_b, Matrix<3, 3> I_b) {
   torque_b_ = 3.0 * mu_e_ / pow(R0, 3.0) * outer_product(normalize(u_b), I_b * normalize(u_b));
   return torque_b_;
 }
 
-Vector<3> GravityGradient::CalcTorque(
-    Vector<3> r_b, Matrix<3, 3> I_b) {  //トルクを取得、引数は前から地球中心方向ベクトル(大きさ付き)、慣性テンソル。引数の単位はそれぞれm,kg*m^2
+Vector<3> GravityGradient::CalcTorque(Vector<3> r_b, Matrix<3, 3> I_b) {
   double coeff = 3.0 * mu_e_ / pow(norm(r_b), 3.0);
   torque_b_ = coeff * outer_product(normalize(r_b), I_b * normalize(r_b));
   return torque_b_;
--- src/Disturbance/GravityGradient.hpp
+++ src/Disturbance/GravityGradient.hpp
@@ -13,11 +13,15 @@ using libra::Vector;
 
 class GravityGradient : public SimpleDisturbance {
  public:
-  GravityGradient();
+  GravityGradient();  // mu is automatically set as earth's gravity
   GravityGradient(const double mu_e_input);
   virtual void Update(const LocalEnvironment& local_env, const Dynamics& dynamics);
+
+  // R0: Earth radius [m], u_b: direction vector to the earth, I_b: Inertia Tensor [kg*m^2]
   Vector<3> CalcTorque(double R0, Vector<3> u_b, Matrix<3, 3> I_b);
+  // r_b: Position vector of the earth [m], I_b: Inertia Tensor [kg*m^2]
   Vector<3> CalcTorque(Vector<3> r_b, Matrix<3, 3> I_b);
+
   virtual std::string GetLogHeader() const;
   virtual std::string GetLogValue() const;
 
