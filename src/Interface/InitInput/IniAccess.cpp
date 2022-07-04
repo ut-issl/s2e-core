c13362c9e230d64a8554062bfc3bf3170aa0b214
sksat <sksat@arkedgespace.com>
2022-07-04 18:52:25 +0900

    fix nest

--- src/Interface/InitInput/IniAccess.cpp
+++ src/Interface/InitInput/IniAccess.cpp
@@ -17,12 +17,14 @@ IniAccess::IniAccess(string path) : file_path_(path) {
 #else
 IniAccess::IniAccess(string path) : file_path_(path), reader(path) {
   std::string ext = ".ini";
-  if (path.size() > 4 && std::equal(std::rbegin(ext), std::rend(ext), std::rbegin(path))) {
-    if (reader.ParseError() != 0) {
-      cout << "Error reading INI file : " << path << endl;
-      cout << "\t error code: " << reader.ParseError() << endl;
-      throw "Error reading INI file";
-    }
+  if(path.size() < 4 || !std::equal(std::rbegin(ext), std::rend(ext), std::rbegin(path))) {
+    // this is not ini file(csv)
+    return;
+  }
+  if (reader.ParseError() != 0) {
+    cout << "Error reading INI file : " << path << endl;
+    cout << "\t error code: " << reader.ParseError() << endl;
+    throw "Error reading INI file";
   }
 }
 #endif
