ac423059b7c6d0454d4374140d44451ef1b60d9b
Satoshi Ikari <ikari@space.t.u-tokyo.ac.jp>
2022-07-19 20:13:53 +0200

    Fix RelativeInformation to add Update function

--- src/RelativeInformation/RelativeInformation.h
+++ src/RelativeInformation/RelativeInformation.h
@@ -9,26 +9,45 @@ class RelativeInformation : public ILoggable {
  public:
   RelativeInformation();
   ~RelativeInformation();
-  virtual std::string GetLogHeader() const;
-  virtual std::string GetLogValue() const;
-  void LogSetup(Logger& logger);
+
+  void Update();
   void RegisterDynamicsInfo(const int sat_id, const Dynamics* dynamics);
   void RemoveDynamicsInfo(const int sat_id);
 
+  // ILoggable
+  virtual std::string GetLogHeader() const;
+  virtual std::string GetLogValue() const;
+  void LogSetup(Logger& logger);
+
   // Getter
-
-  // Transformation Quaternion: Calculate a quaternion represents the coordinate
-  // transformation from the body frame of reference satellite to the body frame
-  // of target satellite target_sat_id: satellite ID in relative motion
-  // reference_sat_id: satellite ID to observe relative motion
-  const libra::Quaternion CalcRelativeAttitudeQuaternion(const int target_sat_id, const int reference_sat_id) const;
-
-  const libra::Vector<3> GetRelativePosition_i(const int target_sat_id, const int reference_sat_id) const;
-  const libra::Vector<3> GetRelativeVelocity_i(const int target_sat_id, const int reference_sat_id) const;
-  libra::Vector<3> CalcRelativePosition_rtn(const int target_sat_id, const int reference_sat_id) const;
+  inline libra::Quaternion GetRelativeAttitudeQuaternion(const int target_sat_id, const int reference_sat_id) const {
+    return rel_att_quaternion_list_[target_sat_id][reference_sat_id];
+  }
+  inline libra::Vector<3> GetRelativePosition_i_m(const int target_sat_id, const int reference_sat_id) const {
+    return rel_pos_list_i_m_[target_sat_id][reference_sat_id];
+  }
+  inline libra::Vector<3> GetRelativeVelocity_i_m_s(const int target_sat_id, const int reference_sat_id) const {
+    return rel_vel_list_i_m_s_[target_sat_id][reference_sat_id];
+  }
+  inline double GetRelativeDistance_m(const int target_sat_id, const int reference_sat_id) const {
+    return rel_distance_list_m_[target_sat_id][reference_sat_id];
+  };
+  inline libra::Vector<3> GetRelativePosition_rtn_m(const int target_sat_id, const int reference_sat_id) const {
+    return rel_pos_list_rtn_m_[target_sat_id][reference_sat_id];
+  }
 
   inline const Dynamics* GetReferenceSatDynamics(const int reference_sat_id) const { return dynamics_database_.at(reference_sat_id); };
 
  private:
   std::map<const int, const Dynamics*> dynamics_database_;
+
+  std::vector<std::vector<libra::Vector<3>>> rel_pos_list_i_m_;
+  std::vector<std::vector<libra::Vector<3>>> rel_vel_list_i_m_s_;
+  std::vector<std::vector<libra::Vector<3>>> rel_pos_list_rtn_m_;
+  std::vector<std::vector<double>> rel_distance_list_m_;
+  std::vector<std::vector<libra::Quaternion>> rel_att_quaternion_list_;
+
+  libra::Quaternion CalcRelativeAttitudeQuaternion(const int target_sat_id, const int reference_sat_id);
+  libra::Vector<3> CalcRelativePosition_rtn_m(const int target_sat_id, const int reference_sat_id);
+  void ResizeLists();
 };
