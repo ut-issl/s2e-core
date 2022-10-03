84c8d119868ef01c5244688cc53f84495967f634
Kazuma Fujimoto <fujimoto-kazuma076@g.ecc.u-tokyo.ac.jp>
2022-10-03 16:34:09 +0900

    Add PCU prescaler

---
+++ src/Component/Power/InitPcu_InitialStudy.cpp
@@ -0,0 +1,24 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include "InitPcu_InitialStudy.hpp"
+
+#include <string>
+#include <vector>
+
+#include "Interface/InitInput/IniAccess.h"
+
+PCU_InitialStudy InitPCU_InitialStudy(ClockGenerator* clock_gen, int pcu_id, const std::string fname, const std::vector<SAP*> saps, BAT* bat, double compo_step_time) {
+  IniAccess pcu_conf(fname);
+
+  const std::string st_pcu_id = std::to_string(static_cast<long long>(pcu_id));
+  const char* cs = st_pcu_id.data();
+
+  char Section[30] = "PCU";
+  strcat(Section, cs);
+
+  int prescaler = pcu_conf.ReadInt(Section, "prescaler");
+  if (prescaler <= 1) prescaler = 1;
+
+  PCU_InitialStudy pcu(prescaler, clock_gen, saps, bat, compo_step_time);
+
+  return pcu;
+}
