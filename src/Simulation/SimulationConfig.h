a01332735816be2f48cf31f963a20ad0e760b48f
Satoshi Ikari <ikari@space.t.u-tokyo.ac.jp>
2022-11-29 09:47:44 +0100

    Fix comments in SimulationConfig.h

--- src/Simulation/SimulationConfig.h
+++ src/Simulation/SimulationConfig.h
@@ -1,17 +1,30 @@
+/**
+ * @file SimulationConfig.h
+ * @brief Definition of struct for simulation setting information
+ */
+
 #pragma once
 #include <string>
 #include <vector>
 
 #include "../Interface/LogOutput/Logger.h"
 
+/**
+ * @struct SimulationConfig
+ * @brief Simulation setting information
+ */
 struct SimulationConfig {
-  std::string ini_base_fname_;
-  Logger* main_logger_;
-  int num_of_simulated_spacecraft_;
-  std::vector<std::string> sat_file_;
-  std::string gs_file_;
-  std::string inter_sat_comm_file_;
-  std::string gnss_file_;
+  std::string ini_base_fname_;         //!< Base file name for initialization
+  Logger* main_logger_;                //!< Main logger
+  int num_of_simulated_spacecraft_;    //!< Number of simulated spacecraft
+  std::vector<std::string> sat_file_;  //!< File name list for spacecraft initialization
+  std::string gs_file_;                //!< File name for ground station initialization
+  std::string inter_sat_comm_file_;    //!< File name for inter-satellite communication initialization
+  std::string gnss_file_;              //!< File name for GNSS initialization
 
+  /**
+   * @fn ~SimulationConfig
+   * @brief Deconstructor
+   */
   ~SimulationConfig() { delete main_logger_; }
 };
