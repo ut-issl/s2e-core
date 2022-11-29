82fe660fbb3e25a0b7f95d5d8e51247e7464a5df
Satoshi Ikari <ikari@space.t.u-tokyo.ac.jp>
2022-11-29 12:52:25 +0100

    Fix comments in InitStructure

--- src/Simulation/Spacecraft/Structure/InitStructure.hpp
+++ src/Simulation/Spacecraft/Structure/InitStructure.hpp
@@ -1,7 +1,24 @@
+/**
+ * @file InitStructure.hpp
+ * @brief Initialize functions for spacecraft structure
+ */
+
 #pragma once
 
 #include <Simulation/Spacecraft/Structure/Structure.h>
 
+/**
+ * @fn InitKinematicsParams
+ * @brief Initialize the kinematics parameters with an ini file
+ */
 KinematicsParams InitKinematicsParams(std::string ini_path);
+/**
+ * @fn InitSurfaces
+ * @brief Initialize the multiple surfaces with an ini file
+ */
 vector<Surface> InitSurfaces(std::string ini_path);
+/**
+ * @fn InitRMMParams
+ * @brief Initialize the RMM(Residual Magnetic Moment) parameters with an ini file
+ */
 RMMParams InitRMMParams(std::string ini_path);
