e45e78dcd54a30f1ca397010681cc63b076df027
Satoshi Ikari <ikari@space.t.u-tokyo.ac.jp>
2022-11-29 11:04:37 +0100

    Fix comments in Sample_PortConfig

--- src/Simulation/Spacecraft/SampleSpacecraft/Sample_PortConfig.h
+++ src/Simulation/Spacecraft/SampleSpacecraft/Sample_PortConfig.h
@@ -1,13 +1,27 @@
-#pragma once
-// コンポとポート番号の対応表
+/**
+ * @file Sample_PortConfig.h
+ * @brief An example of port configuration management
+ */
 
+#pragma once
+
+/**
+ * @enum PowerPortConfig
+ * @brief ID list of electrical power switch ports
+ * @details Register sequential same with port_id. Use NONE_1, NONE_2 if the number is skipped.
+ */
 enum PowerPortConfig {
-  // port_idの順に登録してください。番号がかけている場合はNONE_1,NONE_2…として書いてください
-  // COMPONENT_MAXは消さず、その上に全てのポートに対応するコンポを書いてください
   OBC_BUS,
   GYRO_5V,
-  COMPONENT_MAX
+  COMPONENT_MAX  //!< Maximum port number. Do not remove. Place on the bottom.
 };
 
-// これはOBCソフトウェア側との対応と合わせること！
-enum UARTPortConfig { GYRO = 0, UART_COMPONENT_MAX };
+/**
+ * @enum UARTPortConfig
+ * @brief ID list of serial communication ports with UART
+ * @details Register sequential same with port_id. Use NONE_1, NONE_2 if the number is skipped.
+ */
+enum UARTPortConfig {
+  GYRO = 0,
+  UART_COMPONENT_MAX  //!< Maximum port number. Do not remove. Place on the bottom.
+};
