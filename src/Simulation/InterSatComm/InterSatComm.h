ebee56ee6ce40fe0a2ab072f99a4b1c4f6768ba1
Satoshi Ikari <ikari@space.t.u-tokyo.ac.jp>
2022-11-30 20:26:13 +0100

    Fix comment in InterSatComm

--- src/Simulation/InterSatComm/InterSatComm.h
+++ src/Simulation/InterSatComm/InterSatComm.h
@@ -1,9 +1,27 @@
+/**
+ * @file InterSatComm.h
+ * @brief Base class of inter satellite communication
+ */
+
 #pragma once
+
 #include "../SimulationConfig.h"
 
+/**
+ * @class InterSatComm
+ * @brief Base class of inter satellite communication
+ */
 class InterSatComm {
  public:
+  /**
+   * @fn InterSatComm
+   * @brief Constructor
+   */
   InterSatComm(const SimulationConfig* sim_config);
+  /**
+   * @fn ~InterSatComm
+   * @brief Deconstructor
+   */
   ~InterSatComm();
 
  private:
