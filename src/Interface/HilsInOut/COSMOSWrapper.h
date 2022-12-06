a085315ac321d87b77087110b5c3ebdbd8199e04
Satoshi Ikari <ikari@space.t.u-tokyo.ac.jp>
2022-12-06 14:17:26 +0100

    Fix comment in COSMOSWrapper

--- src/Interface/HilsInOut/COSMOSWrapper.h
+++ src/Interface/HilsInOut/COSMOSWrapper.h
@@ -1,23 +1,75 @@
-#pragma once
+/**
+ * @file COSMOSWrapper.h
+ * @brief Wrapper for COSMOS
+ * @details COSMOS Scripting Guide: https://cosmosrb.com/docs/scripting/
+ * @note This file is very old and recently not managed well...
+ */
 
+#pragma once
 #include <string>
 
 #include "COSMOS_TCP_IF.h"
-
-class COSMOSWrapper : private COSMOS_TCP_IF  // SendString関数を使う目的で継承
-{
+/**
+ * @class COSMOSWrapper
+ * @brief Wrapper for COSMOS
+ */
+class COSMOSWrapper : private COSMOS_TCP_IF {
  public:
+  /**
+   * @fn COSMOSWrapper
+   * @brief Constructor.
+   */
   COSMOSWrapper(bool enable);
+  /**
+   * @fn COSMOSWrapper
+   * @brief Constructor.
+   */
   COSMOSWrapper(unsigned short port_num, bool enable, char* server_ip);
-  COSMOSWrapper();  // when not in use
+  /**
+   * @fn COSMOSWrapper
+   * @brief Default Constructor. When not in use
+   */
+  COSMOSWrapper();
+  /**
+   * @fn ~COSMOSWrapper
+   * @brief Destructor.
+   */
   ~COSMOSWrapper();
+
+  /**
+   * @fn Cmd
+   * @brief Wrapper of cmd function to send data from COSMOS to RDP
+   * @param [in] cmd_str: String of command
+   */
   void Cmd(std::string cmd_str);
+  /**
+   * @fn Cmd_RDP_SAT_CMD_EXTERNAL_TORQUE
+   * @brief Command to set external torque
+   */
   void Cmd_RDP_SAT_CMD_EXTERNAL_TORQUE(uint8_t torque_frame, uint32_t duration_ms, float ext_torque_x, float ext_torque_y, float ext_torque_z);
+
+  /**
+   * @fn Initialize
+   * @brief Initialize the TCP connection with COSMOS
+   */
   void Initialize();
+  /**
+   * @fn Finalize
+   * @brief Finalize the TCP connection with COSMOS
+   */
   void Finalize();
+  /**
+   * @fn Enable
+   * @brief Return enable flag
+   */
   bool Enable() const;
 
  private:
+  /**
+   * @fn CosmosEval
+   * @brief Send string to COSMOS
+   */
   void CosmosEval(std::string expr);
-  const bool enable_;
+
+  const bool enable_;  //!< Enable flag
 };
