f878a473011a8e66ed3ede30ee78f24a4e5936b4
Satoshi Ikari <ikari@space.t.u-tokyo.ac.jp>
2022-12-09 20:45:55 +0100

    Fix comments in ClockGenerator

--- src/Environment/Global/ClockGenerator.h
+++ src/Environment/Global/ClockGenerator.h
@@ -1,3 +1,8 @@
+/**
+ * @file ClockGenerator.h
+ * @brief Class to generate clock for classes which have ITickable
+ */
+
 #pragma once
 #include <Component/Abstract/ITickable.h>
 
@@ -5,23 +10,50 @@
 
 #include "SimTime.h"
 
-// ITickableを実装したクラスにクロック（Tick）を与えるクラス
-// メインループなどで周期的にTickToComponentsを呼び出すという想定
+/**
+ * @class ClockGenerator
+ * @brief Class to generate clock for classes which have ITickable
+ */
 class ClockGenerator {
  public:
+  /**
+   * @fn ~ClockGenerator
+   * @brief Destructor
+   */
   ~ClockGenerator();
 
-  // クロックの周期（ミリ秒）
-  const int IntervalMillisecond = 1;
-
+  /**
+   * @fn RegisterComponent
+   * @brief Register component which has ITickable
+   * @param [in] ticlable: Component class
+   */
   void RegisterComponent(ITickable* tickable);
+  /**
+   * @fn RemoveComponent
+   * @brief Removed registered component
+   * @param [in] ticlable: Registered component class
+   */
   void RemoveComponent(ITickable* tickable);
+  /**
+   * @fn TickToComponents
+   * @brief Execute tick function of all registered components
+   */
   void TickToComponents();
+  /**
+   * @fn UpdateComponents
+   * @brief Execute TickToComponents when component update timing
+   * @param [in] sim_time: Simulation time
+   */
   void UpdateComponents(const SimTime* sim_time);
-
+  /**
+   * @fn ClearTimerCount
+   * @brief Clear time count
+   */
   inline void ClearTimerCount(void) { timer_count_ = 0; }
 
+  const int IntervalMillisecond = 1;  //!< Clock period [ms]. (Currenly, this is not used. TODO: Delete this.)
+
  private:
-  std::vector<ITickable*> components_;
-  int timer_count_; // TODO consider size, unsigned
+  std::vector<ITickable*> components_;  //!< Component list fot tick
+  int timer_count_;                     //!< Timer count TODO: consider size, unsigned
 };
