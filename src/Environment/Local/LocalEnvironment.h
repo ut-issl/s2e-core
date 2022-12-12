0127bc1d65c84bba012ed3c2d05e83c2f235d4d6
Satoshi Ikari <ikari@space.t.u-tokyo.ac.jp>
2022-12-12 15:19:42 +0100

    Fix comment in LocalEnvironment

--- src/Environment/Local/LocalEnvironment.h
+++ src/Environment/Local/LocalEnvironment.h
@@ -22,21 +22,66 @@ class SimTime;
  */
 class LocalEnvironment {
  public:
+  /**
+   * @fn LocalEnvironment
+   * @brief Constructor
+   * @param [in] sim_config: Simulation configuration
+   * @param [in] glo_env: Global environment
+   * @param [in] sat_id: Satellite ID
+   */
   LocalEnvironment(SimulationConfig* sim_config, const GlobalEnvironment* glo_env, const int sat_id);
+  /**
+   * @fn ~LocalEnvironment
+   * @brief Destructor
+   */
   ~LocalEnvironment();
+  /**
+   * @fn Initialize
+   * @brief Initialize function
+   * @param [in] sim_config: Simulation configuration
+   * @param [in] glo_env: Global environment
+   * @param [in] sat_id: Satellite ID
+   */
   void Initialize(SimulationConfig* sim_config, const GlobalEnvironment* glo_env, const int sat_id);
+
+  /**
+   * @fn Update
+   * @brief Update all states
+   * @param [in] dynamics: Dynamics information of the satellite
+   * @param [in] sim_time: Simulation time
+   */
   void Update(const Dynamics* dynamics, const SimTime* sim_time);
+
+  /**
+   * @fn LogSetup
+   * @brief Log setup for local environments
+   */
   void LogSetup(Logger& logger);
 
-  // Get functions
+  /**
+   * @fn GetAtmosphere
+   * @brief Return Atmosphere class
+   */
   inline const Atmosphere& GetAtmosphere() const { return *atmosphere_; }
+  /**
+   * @fn GetMag
+   * @brief Return MagEnvironment class
+   */
   inline const MagEnvironment& GetMag() const { return *mag_; }
+  /**
+   * @fn GetSrp
+   * @brief Return SRPEnvironment class
+   */
   inline const SRPEnvironment& GetSrp() const { return *srp_; }
+  /**
+   * @fn GetCelesInfo
+   * @brief Return LocalCelestialInformation class
+   */
   inline const LocalCelestialInformation& GetCelesInfo() const { return *celes_info_; }
 
  private:
-  Atmosphere* atmosphere_;
-  MagEnvironment* mag_;
-  SRPEnvironment* srp_;
-  LocalCelestialInformation* celes_info_;
+  Atmosphere* atmosphere_;                 //!< Atmospheric density of the earth
+  MagEnvironment* mag_;                    //!< Magnetic field of the earth
+  SRPEnvironment* srp_;                    //!< Solar radiation pressure
+  LocalCelestialInformation* celes_info_;  //!< Celestial information
 };
