b00577c4467c9ebc4f72777033b51e975c6cb0b5
Satoshi Ikari <ikari@space.t.u-tokyo.ac.jp>
2022-12-13 14:03:34 +0100

    Fix comments in GlobalRand

--- src/Library/math/GlobalRand.h
+++ src/Library/math/GlobalRand.h
@@ -1,19 +1,41 @@
+/**
+ * @file GlobalRand.h
+ * @brief Class to manage global randomization
+ */
+
 #ifndef GLOBALRAND_HPP_
 #define GLOBALRAND_HPP_
 #include "./Ran0.hpp"
 
+/**
+ * @class GlobalRand.h
+ * @brief Class to manage global randomization
+ * @note Used to make randomized seed for other randomization
+ */
 class GlobalRand {
  public:
+  /**
+   * @fn GlobalRand
+   * @brief Constructor
+   */
   GlobalRand();
+  /**
+   * @fn SetSeed
+   * @brief Set randomized seed value
+   */
   void SetSeed(long seed);
+  /**
+   * @fn MakeSeed
+   * @brief Set randomized seed value
+   */
   long MakeSeed();
 
  private:
-  static const unsigned int MAX_SEED = 0xffffffff;
-  libra::Ran0 base_rand_;
-  long seed_;
+  static const unsigned int MAX_SEED = 0xffffffff;  //!< Maximum value of seed
+  libra::Ran0 base_rand_;                           //!< Base of global randomization
+  long seed_;                                       //!< Seed of global randomization
 };
 
-extern GlobalRand g_rand;
+extern GlobalRand g_rand;  //!< Global randomization
 
 #endif
