a5e37c22729e157c4aea48399ecd05f27b9b58e8
Satoshi Ikari <ikari@space.t.u-tokyo.ac.jp>
2022-12-14 10:21:31 +0100

    Fix comments in ExpHils

--- src/Component/Abstract/ExpHils.h
+++ src/Component/Abstract/ExpHils.h
@@ -1,36 +1,69 @@
+/**
+ * @file ExpHils.h
+ * @brief Example of component emulation for UART communication in HILS environment
+ */
 #pragma once
 #include <vector>
 
 #include "ComponentBase.h"
 #include "ObcCommunicationBase.h"
 
-// mock components for training
-// # Specification for ExpHils
-// * Checking UART communication for HILS exam
-//   compo_id = 0 : the sender compo which sends out a new massage
-//   compo_id = 1 : the responder compo which returns the message as received
-// * message size is 4 bytes
-//   the first 3 bytes : ASCII(ABC, BCD, CDE,...)
-//   the last byte : \0
-
+/**
+ * @class ExpHils
+ * @brief Example of component emulation for communication in HILS environment
+ * @details The sender mode: ExpHils sends out a new massage
+ *          - message size is 4 bytes
+ *            - the first 3 bytes : ASCII(ABC, BCD, CDE,...)
+ *            - the last byte : \0
+ *          The responder mode: ExpHils returns the message as received
+ */
 class ExpHils : public ComponentBase, public ObcCommunicationBase {
  public:
+  /**
+   * @fn ExpHils
+   * @brief Constructor
+   * @note prescaler is set as 300.
+   * @param [in] clock_gen: Clock generator
+   * @param [in] sils_port_id: Port ID for communication line b/w OBC
+   * @param [in] obc: The communication target OBC
+   * @param [in] hils_port_id: ID of HILS communication port
+   * @param [in] baud_rate: Baud rate of HILS communication port
+   * @param [in] hils_port_manager: HILS port manager
+   * @param [in] mode_id: Mode ID to select sender(0) or responder(1)
+   */
   ExpHils(ClockGenerator* clock_gen, const int sils_port_id, OBC* obc, const unsigned int hils_port_id, const unsigned int baud_rate,
           HilsPortManager* hils_port_manager, const int mode_id);
+  /**
+   * @fn ~ExpHils
+   * @brief Destructor
+   */
   ~ExpHils();
 
  protected:
+  // Override functions for ComponentBase
+  /**
+   * @fn MainRoutine
+   * @brief Main routine to receive command and send telemetry
+   */
   void MainRoutine(int count);
 
  private:
-  const static int kMemorySize = 4;
-  const int kNumAlphabet = 26;
-  char memory_[kMemorySize] = {0, 0, 0, '\0'};  // for the responder compo
-  char tx_[kMemorySize] = {0, 0, 0, '\0'};
-  const int mode_id_;
-  int counter_ = 0;
+  const static int kMemorySize = 4;             //!< Memory size
+  const int kNumAlphabet = 26;                  //!< Number of Alphabet
+  char memory_[kMemorySize] = {0, 0, 0, '\0'};  //!< Memory
+  char tx_[kMemorySize] = {0, 0, 0, '\0'};      //!< TX(Telemetry send) buffer
+  const int mode_id_;                           //!<  Mode ID to select sender(0) or responder(1)
+  int counter_ = 0;                             //!< Internal counter
 
-  // override ObcComunication
+  // Override functions for ObcComunication
+  /**
+   * @fn ParseCommand
+   * @brief Parse command received from OBC
+   */
   int ParseCommand(const int cmd_size) override;
+  /**
+   * @fn GenerateTelemetry
+   * @brief Generate telemetry send to OBC
+   */
   int GenerateTelemetry() override;
 };
