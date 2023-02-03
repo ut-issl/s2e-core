e8fecbcdf955521e18e3d7089f82b7f103f584eb
Satoshi Ikari <ikari@space.t.u-tokyo.ac.jp>
2023-02-03 10:54:48 +0100

    Fix ini file for battery

--- src/Component/Power/InitBat.cpp
+++ src/Component/Power/InitBat.cpp
@@ -32,24 +32,24 @@ BAT InitBAT(ClockGenerator* clock_gen, int bat_id, const std::string fname, doub
   cell_capacity = bat_conf.ReadDouble(Section, "cell_capacity");
 
   int approx_order;
-  approx_order = bat_conf.ReadInt(Section, "approx_order");
+  approx_order = bat_conf.ReadInt(Section, "approximation_order");
 
   std::vector<double> cell_discharge_curve_coeffs;
   for (int i = 0; i <= approx_order; ++i) {
-    cell_discharge_curve_coeffs.push_back(bat_conf.ReadDouble(Section, ("cell_discharge_curve_coeffs(" + std::to_string(i) + ")").c_str()));
+    cell_discharge_curve_coeffs.push_back(bat_conf.ReadDouble(Section, ("cell_discharge_curve_coefficients(" + std::to_string(i) + ")").c_str()));
   }
 
   double initial_dod;
   initial_dod = bat_conf.ReadDouble(Section, "initial_dod");
 
   double cc_charge_c_rate;
-  cc_charge_c_rate = bat_conf.ReadDouble(Section, "cc_charge_c_rate");
+  cc_charge_c_rate = bat_conf.ReadDouble(Section, "constant_charge_current_rate_C");
 
   double cv_charge_voltage;
-  cv_charge_voltage = bat_conf.ReadDouble(Section, "cv_charge_voltage");
+  cv_charge_voltage = bat_conf.ReadDouble(Section, "constant_voltage_charge_voltage_V");
 
   double bat_resistance;
-  bat_resistance = bat_conf.ReadDouble(Section, "bat_resistance");
+  bat_resistance = bat_conf.ReadDouble(Section, "battery_resistance_Ohm");
 
   BAT bat(prescaler, clock_gen, number_of_series, number_of_parallel, cell_capacity, cell_discharge_curve_coeffs, initial_dod, cc_charge_c_rate,
           cv_charge_voltage, bat_resistance, compo_step_time);
