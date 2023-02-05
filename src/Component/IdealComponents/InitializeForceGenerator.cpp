1d60427aed8fe0881548fba4aebfd280e88b989a
Satoshi Ikari <ikari@space.t.u-tokyo.ac.jp>
2023-02-05 12:52:07 +0100

    Fix to use SNAKE_CASE for force generator

--- src/Component/IdealComponents/InitializeForceGenerator.cpp
+++ src/Component/IdealComponents/InitializeForceGenerator.cpp
@@ -11,11 +11,11 @@ ForceGenerator InitializeForceGenerator(ClockGenerator* clock_gen, const std::st
   IniAccess ini_file(file_name);
 
   // CompoBase
-  int prescaler = ini_file.ReadInt("ComponentBase", "prescaler");
+  int prescaler = ini_file.ReadInt("COMPONENT_BASE", "prescaler");
   if (prescaler <= 1) prescaler = 1;
 
   // ForceGenerator
-  char section[30] = "ForceGenerator";
+  char section[30] = "FORCE_GENERATOR";
   double force_magnitude_standard_deviation_N = ini_file.ReadDouble(section, "force_magnitude_standard_deviation_N");
   double force_direction_standard_deviation_deg = ini_file.ReadDouble(section, "force_direction_standard_deviation_deg");
   double force_direction_standard_deviation_rad = libra::deg_to_rad * force_direction_standard_deviation_deg;
