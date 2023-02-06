f164e9622f00a39fc3150f23341840a4d0e2e42f
Satoshi Ikari <ikari@space.t.u-tokyo.ac.jp>
2023-02-06 14:28:47 +0100

    Fix format

--- src/Component/AOCS/RWModel.h
+++ src/Component/AOCS/RWModel.h
@@ -54,12 +54,13 @@ class RWModel : public ComponentBase, public ILoggable {
    * @param [in] drive_flag: RW drive flag
    * @param [in] init_velocity: Initial value of angular velocity of RW
    */
-  RWModel(const int prescaler, const int fast_prescaler, ClockGenerator *clock_gen, const int id, const double step_width, const double dt_main_routine,
-          const double jitter_update_interval, const double inertia, const double max_torque, const double max_velocity_rpm, const Quaternion q_b2c,
-          const Vector<3> pos_b, const double dead_time, const Vector<3> driving_lag_coef, const Vector<3> coasting_lag_coef,
-          bool is_calc_jitter_enabled, bool is_log_jitter_enabled, std::vector<std::vector<double>> radial_force_harmonics_coef,
-          std::vector<std::vector<double>> radial_torque_harmonics_coef, double structural_resonance_freq, double damping_factor, double bandwidth,
-          bool considers_structural_resonance, const bool drive_flag = false, const double init_velocity = 0.0);
+  RWModel(const int prescaler, const int fast_prescaler, ClockGenerator *clock_gen, const int id, const double step_width,
+          const double dt_main_routine, const double jitter_update_interval, const double inertia, const double max_torque,
+          const double max_velocity_rpm, const Quaternion q_b2c, const Vector<3> pos_b, const double dead_time, const Vector<3> driving_lag_coef,
+          const Vector<3> coasting_lag_coef, bool is_calc_jitter_enabled, bool is_log_jitter_enabled,
+          std::vector<std::vector<double>> radial_force_harmonics_coef, std::vector<std::vector<double>> radial_torque_harmonics_coef,
+          double structural_resonance_freq, double damping_factor, double bandwidth, bool considers_structural_resonance,
+          const bool drive_flag = false, const double init_velocity = 0.0);
   /**
    * @fn RWModel
    * @brief Constructor with power port
