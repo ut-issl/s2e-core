30a5ea1797a03e3b5e0acacbccdfc42e1d486963
Satoshi Ikari <ikari@space.t.u-tokyo.ac.jp>
2023-02-07 11:14:32 +0100

    Fix include guard for initialize_monte_carlo_simulation.hpp

--- src/simulation/monte_carlo_simulation/initialize_monte_carlo_simulation.hpp
+++ src/simulation/monte_carlo_simulation/initialize_monte_carlo_simulation.hpp
@@ -3,7 +3,8 @@
  * @brief Initialize function for Monte-Carlo Simulator
  */
 
-#pragma once
+#ifndef S2E_SIMULATION_MONTE_CARLO_SIMULATION_INITIALIZE_MONTE_CARLO_SIMULATION_H_
+#define S2E_SIMULATION_MONTE_CARLO_SIMULATION_INITIALIZE_MONTE_CARLO_SIMULATION_H_
 
 #include "initialize_monte_carlo_parameters.hpp"
 #include "monte_carlo_simulation_executor.hpp"
@@ -13,3 +14,5 @@
  * @brief Initialize function for Monte-Carlo Simulator
  */
 MCSimExecutor* InitMCSim(std::string file_name);
+
+#endif  // S2E_SIMULATION_MONTE_CARLO_SIMULATION_INITIALIZE_MONTE_CARLO_SIMULATION_H_
