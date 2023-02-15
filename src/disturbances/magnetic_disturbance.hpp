cb37b5b6a850afeda51bb7a2e66cfb5f7867d885
Satoshi Ikari <ikari@space.t.u-tokyo.ac.jp>
2023-02-15 15:00:33 +0100

    Refactor mag disturbance

--- src/disturbances/magnetic_disturbance.hpp
+++ src/disturbances/magnetic_disturbance.hpp
@@ -20,21 +20,11 @@ class MagDisturbance : public SimpleDisturbance {
   /**
    * @fn MagDisturbance
    * @brief Constructor
+   * @param [in] rmm_parameters: RMM parameters of the spacecraft
    * @param [in] is_calculation_enabled: Calculation flag
    */
-  MagDisturbance(const RMMParams& rmm_params, const bool is_calculation_enabled = true);
+  MagDisturbance(const RMMParams& rmm_parameters, const bool is_calculation_enabled = true);
 
-  /**
-   * @fn CalcRMM
-   * @brief Calculate true RMM of the spacecraft
-   */
-  void CalcRMM();
-  /**
-   * @fn CalcTorque
-   * @brief Calculate magnetic disturbance torque
-   * @param [in] magnetic_field_b_nT: Magnetic field vector at the body frame [nT]
-   */
-  libra::Vector<3> CalcTorque(const libra::Vector<3>& magnetic_field_b_nT);
   /**
    * @fn Update
    * @brief Override Updates function of SimpleDisturbance
@@ -58,6 +48,18 @@ class MagDisturbance : public SimpleDisturbance {
 
   libra::Vector<3> rmm_b_Am2_;   //!< True RMM of the spacecraft in the body frame [Am2]
   const RMMParams& rmm_params_;  //!< RMM parameters
+
+  /**
+   * @fn CalcRMM
+   * @brief Calculate true RMM of the spacecraft
+   */
+  void CalcRMM();
+  /**
+   * @fn CalcTorque
+   * @brief Calculate magnetic disturbance torque
+   * @param [in] magnetic_field_b_nT: Magnetic field vector at the body frame [nT]
+   */
+  libra::Vector<3> CalcTorque(const libra::Vector<3>& magnetic_field_b_nT);
 };
 
 #endif  // S2E_DISTURBANCES_MAGNETIC_DISTURBANCE_HPP_
