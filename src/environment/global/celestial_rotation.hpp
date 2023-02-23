5ab4bc06c5656fd40bbb4eccef812efedaf760ab
Satoshi Ikari <ikari@space.t.u-tokyo.ac.jp>
2023-02-23 10:27:51 +0100

    Fix member variable names

--- src/environment/global/celestial_rotation.hpp
+++ src/environment/global/celestial_rotation.hpp
@@ -10,8 +10,8 @@
 #ifndef S2E_ENVIRONMENT_GLOBAL_CELESTIAL_ROTATION_HPP_
 #define S2E_ENVIRONMENT_GLOBAL_CELESTIAL_ROTATION_HPP_
 
-#include <library/logger/loggable.hpp>
-#include <library/math/matrix.hpp>
+#include "library/logger/loggable.hpp"
+#include "library/math/matrix.hpp"
 
 /**
  * @enum RotationMode
@@ -50,13 +50,13 @@ class CelestialRotation {
    * @fn GetDCMJ2000toXCXF
    * @brief Return the DCM between J2000 inertial frame and the frame of fixed to the target object X (X-Centered X-Fixed)
    */
-  inline const libra::Matrix<3, 3> GetDCMJ2000toXCXF() const { return DCM_J2000toXCXF_; };
+  inline const libra::Matrix<3, 3> GetDCMJ2000toXCXF() const { return dcm_j2000_to_xcxf_; };
 
   /**
    * @fn GetDCMJ2000toXCXF
    * @brief Return the DCM between TEME (Inertial frame used in SGP4) and the frame of fixed to the target object X (X-Centered X-Fixed)
    */
-  inline const libra::Matrix<3, 3> GetDCMTEMEtoXCXF() const { return DCM_TEMEtoXCXF_; };
+  inline const libra::Matrix<3, 3> GetDCMTEMEtoXCXF() const { return dcm_teme_to_xcxf_; };
 
  private:
   /**
@@ -73,34 +73,35 @@ class CelestialRotation {
   libra::Matrix<3, 3> Precession(const double (&tTT_century)[4]);     //!< Movement of the coordinate axes due to Precession
   libra::Matrix<3, 3> PolarMotion(const double Xp, const double Yp);  //!< Movement of the coordinate axes due to Polar Motion
 
-  double dpsi_rad_;                      //!< Nutation in obliquity [rad]
-  double depsilon_rad_;                  //!< Nutation in longitude [rad]
-  double epsi_rad_;                      //!< Mean obliquity of the ecliptic [rad]
-  libra::Matrix<3, 3> DCM_J2000toXCXF_;  //!< Direction Cosine Matrix J2000 to XCXF(X-Centered X-Fixed)
-  libra::Matrix<3, 3> DCM_TEMEtoXCXF_;   //!< Direction Cosine Matrix TEME to XCXF(X-Centered X-Fixed)
-  RotationMode rotation_mode_;           //!< Designation of dynamics model
-  std::string planet_name_;              //!< Designate which solar planet the instance should work as
+  double d_psi_rad_;                       //!< Nutation in obliquity [rad]
+  double d_epsilon_rad_;                   //!< Nutation in longitude [rad]
+  double epsilon_rad_;                     //!< Mean obliquity of the ecliptic [rad]
+  libra::Matrix<3, 3> dcm_j2000_to_xcxf_;  //!< Direction Cosine Matrix J2000 to XCXF(X-Centered X-Fixed)
+  libra::Matrix<3, 3> dcm_teme_to_xcxf_;   //!< Direction Cosine Matrix TEME to XCXF(X-Centered X-Fixed)
+  RotationMode rotation_mode_;             //!< Designation of dynamics model
+  std::string planet_name_;                //!< Designate which solar planet the instance should work as
 
-  // Definitions of coefficeints
+  // Definitions of coefficients
   // They are handling as constant values
   // TODO: Consider to read setting files for these coefficients
   // TODO: Consider other formats for other planets
-  double c_epsi_rad_[4];  //!< Coefficients to compute mean obliquity of the ecliptic
-  double c_lm_rad_[5];    //!< Coefficients to compute delauney angle (l=lm: Mean anomaly of the moon)
-  double c_ls_rad_[5];    //!< Coefficients to compute delauney angle (l'=ls: Mean anomaly of the sun)
-  double c_F_rad_[5];     //!< Coefficients to compute delauney angle (F: Mean longitude of the moon - mean longitude of ascending node of the moon)
-  double c_D_rad_[5];     //!< Coefficients to compute delauney angle (D: Elogation of the moon from the sun)
-  double c_O_rad_[5];     //!< Coefficients to compute delauney angle (Ω=O: Mean longitude of ascending node of the moon)
-  double c_depsilon_rad_[9];  //!< Coefficients to compute nutation angle (delta-epsilon)
-  double c_dpsi_rad_[9];      //!< Coefficients to compute nutation angle (delta-psi)
-  double c_zeta_rad_[3];      //!< Coefficients to compute precession angle (zeta)
-  double c_theta_rad_[3];     //!< Coefficients to compute precession angle (theta)
-  double c_z_rad_[2];         //!< Coefficients to compute precession angle (z)
+  double c_epsilon_rad_[4];  //!< Coefficients to compute mean obliquity of the ecliptic
+  double c_lm_rad_[5];       //!< Coefficients to compute delauney angle (l=lm: Mean anomaly of the moon)
+  double c_ls_rad_[5];       //!< Coefficients to compute delauney angle (l'=ls: Mean anomaly of the sun)
+  double c_f_rad_[5];  //!< Coefficients to compute delauney angle (F: Mean longitude of the moon - mean longitude of ascending node of the moon)
+  double c_d_rad_[5];  //!< Coefficients to compute delauney angle (D: Elogation of the moon from the sun)
+  double c_o_rad_[5];  //!< Coefficients to compute delauney angle (Ω=O: Mean longitude of ascending node of the moon)
+  double c_d_epsilon_rad_[9];  //!< Coefficients to compute nutation angle (delta-epsilon)
+  double c_d_psi_rad_[9];      //!< Coefficients to compute nutation angle (delta-psi)
+  double c_zeta_rad_[3];       //!< Coefficients to compute precession angle (zeta)
+  double c_theta_rad_[3];      //!< Coefficients to compute precession angle (theta)
+  double c_z_rad_[2];          //!< Coefficients to compute precession angle (z)
 
-  const double dtUT1UTC_ = 32.184;                   //!< Time difference b/w UT1 and UTC [sec]
-  const double kSec2Day = 1 / (24.0 * 60.0 * 60.0);  //!< Conversion constant from sec to day
-  const double kJulianDateJ2000 = 2451545.0;         //!< Julian date of J2000 [day]
-  const double kDayJulianCentury = 36525;            //!< Conversion constant from Julian century to day [day/century]
+  // TODO: Move to general constant values
+  const double kDtUt1Utc_ = 32.184;                     //!< Time difference b/w UT1 and UTC [sec]
+  const double kSec2Day_ = 1.0 / (24.0 * 60.0 * 60.0);  //!< Conversion constant from sec to day
+  const double kJulianDateJ2000_ = 2451545.0;           //!< Julian date of J2000 [day]
+  const double kDayJulianCentury_ = 36525.0;            //!< Conversion constant from Julian century to day [day/century]
 };
 
 #endif  // S2E_ENVIRONMENT_GLOBAL_CELESTIAL_ROTATION_HPP_
