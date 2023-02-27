bc0c8a026c2b2a8f7a4ab5154770b5643d295bda
Satoshi Ikari <ikari@space.t.u-tokyo.ac.jp>
2023-02-27 10:51:55 +0100

    Move public const value to private

--- src/components/ports/hils_i2c_target_port.hpp
+++ src/components/ports/hils_i2c_target_port.hpp
@@ -10,9 +10,6 @@
 
 #include "hils_uart_port.hpp"
 
-const int kDefaultCmdSize = 0xff;  //!< Default command size
-const int kDefaultTxSize = 0xff;   //!< Default TX size
-
 /**
  * @class HilsI2cTargetPort
  * @brief Class to control I2C-USB converter for the target(device) side from COM port
@@ -96,6 +93,8 @@ class HilsI2cTargetPort : public HilsUartPort {
   int GetStoredFrameCounter();
 
  private:
+  const unsigned int kDefaultCmdSize = 0xff;     //!< Default command size
+  const unsigned int kDefaultTxSize = 0xff;      //!< Default TX size
   unsigned char max_register_number_ = 0xff;     //!< Maximum register number
   unsigned char saved_register_address_ = 0x00;  //!< Saved register address
   unsigned int stored_frame_counter_ = 0;        //!< Send a few frames of telemetry to the converter in advance.
