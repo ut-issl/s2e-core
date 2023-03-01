603dc2cddb9c1498f492e4863ba718d113494050
Satoshi Ikari <ikari@space.t.u-tokyo.ac.jp>
2023-03-01 14:20:24 +0100

    Add test file for MatrixVector

---
+++ src/library/math/test_matrix_vector.cpp
@@ -0,0 +1,14 @@
+/**
+ * @file test_quaternion.cpp
+ * @brief Test codes for Quaternion class with GoogleTest
+ */
+#include <gtest/gtest.h>
+
+#include "constants.hpp"
+#include "matrix_vector.hpp"
+
+/**
+ * @brief Test for Matrix * Vector
+ */
+TEST(MatrixVector, MultiplyMatrixVector) {
+}
