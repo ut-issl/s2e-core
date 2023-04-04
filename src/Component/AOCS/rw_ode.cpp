a8e14f7fc17bbbcd58550331501208d9794fc94c
LilKoke <k-hirano@wb3.so-net.ne.jp>
2023-04-04 20:10:01 +0900

    fix syntax error

--- src/Component/AOCS/rw_ode.cpp
+++ src/Component/AOCS/rw_ode.cpp
@@ -8,8 +8,8 @@
 
 using namespace libra;
 
-RwOde::RwOde(double step_width, double init_angular_velocity, double target_angular_velocity, Vector<3> lag_coef) ODE<1>(step_width),
-    lag_coef_(lag_coef), kInitAngularVelocity_(init_angular_velocity), target_angular_velocity_(target_angular_velocity) {
+RwOde::RwOde(double step_width, double init_angular_velocity, double target_angular_velocity, Vector<3> lag_coef)
+: ODE<1>(step_width),lag_coef_(lag_coef), kInitAngularVelocity_(init_angular_velocity), target_angular_velocity_(target_angular_velocity) {
   this->setup(0.0, Vector<1>(init_angular_velocity));
 }
 
