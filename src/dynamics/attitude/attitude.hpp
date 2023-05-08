10873816254d96e72dff27e767a95d405a7cdb37
Satoshi Ikari <ikari@space.t.u-tokyo.ac.jp>
2023-05-08 16:09:34 +0200

    Fix format

--- src/dynamics/attitude/attitude.hpp
+++ src/dynamics/attitude/attitude.hpp
@@ -114,11 +114,11 @@ class Attitude : public ILoggable, public SimulationObject {
   virtual void SetParameters(const MonteCarloSimulationExecutor& mc_simulator);
 
  protected:
-  bool is_calc_enabled_ = true;                     //!< Calculation flag
-  double propagation_step_s_;                       //!< Propagation step [sec]
-  libra::Vector<3> angular_velocity_b_rad_s_;       //!< Angular velocity of spacecraft body fixed frame with respect to the inertial frame [rad/s]
-  libra::Quaternion quaternion_i2b_;                //!< Attitude quaternion from the inertial frame to the body fixed frame
-  libra::Vector<3> torque_b_Nm_;                    //!< Torque in the body fixed frame [Nm]
+  bool is_calc_enabled_ = true;                //!< Calculation flag
+  double propagation_step_s_;                  //!< Propagation step [sec]
+  libra::Vector<3> angular_velocity_b_rad_s_;  //!< Angular velocity of spacecraft body fixed frame with respect to the inertial frame [rad/s]
+  libra::Quaternion quaternion_i2b_;           //!< Attitude quaternion from the inertial frame to the body fixed frame
+  libra::Vector<3> torque_b_Nm_;               //!< Torque in the body fixed frame [Nm]
 
   const libra::Matrix<3, 3>& inertia_tensor_kgm2_;  //!< Inertia tensor of the spacecraft [kg m^2]
 
