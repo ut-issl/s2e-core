c651b588eb698d8f4c1e4c8685abf3e58a3eff14
Satoshi Ikari <ikari@space.t.u-tokyo.ac.jp>
2023-06-16 15:31:31 +0200

    Add run_with_work

--- scripts/Docker_Ubuntu/setup_docker.sh
+++ scripts/Docker_Ubuntu/setup_docker.sh
@@ -10,18 +10,19 @@ function build {
     cd ./Docker_Ubuntu
 }
 
-# function run {
-#     echo "イメージからコンテナを作成します"
-#     docker run -it -d --cap-add=SYS_PTRACE --security-opt="seccomp=unconfined" -v /$(realpath ../../../../work):/home/s2e/work --name issl-1 -p 2222:22 issl:latest
-# }
-
 function enter {
     echo "コンテナ内のbashにアタッチします"
     winpty docker run -it --rm -p 2222:22 -v /$(realpath ../../../../work):/home/s2e/work issl:latest /bin/bash
 }
 
 function run_core {
+    echo "s2e-coreのみをシェアするイメージからコンテナを作成します"
     docker run -it -d --cap-add=SYS_PTRACE --security-opt="seccomp=unconfined" -v /$(realpath ../../../s2e-core):/home/s2e/work/s2e-core --name issl-1 -p 2222:22 issl:latest
+}
+
+function run_with_work {
+    echo "workディレクトリをシェアするイメージからコンテナを作成します"
+    docker run -it -d --cap-add=SYS_PTRACE --security-opt="seccomp=unconfined" -v /$(realpath ../../../../work):/home/s2e/work --name issl-1 -p 2222:22 issl:latest
 }
 
 subcommand="$1"
