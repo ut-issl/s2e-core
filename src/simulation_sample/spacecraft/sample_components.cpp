c1fc2c2767bfa60442b0295171e3c8ad0c6c0a20
Satoshi Ikari <ikari@space.t.u-tokyo.ac.jp>
2023-06-28 00:27:53 +0200

    Fix reference time information

--- src/simulation_sample/spacecraft/sample_components.cpp
+++ src/simulation_sample/spacecraft/sample_components.cpp
@@ -71,7 +71,7 @@ SampleComponents::SampleComponents(const Dynamics* dynamics, Structure* structur
   file_name = iniAccess.ReadString("COMPONENT_FILES", "rw_file");
   configuration_->main_logger_->CopyFileToLogDirectory(file_name);
   reaction_wheel_ =
-      new ReactionWheel(InitReactionWheel(clock_generator, pcu_->GetPowerPort(2), 1, file_name, dynamics_->GetAttitude().GetPropStep_s(),
+      new ReactionWheel(InitReactionWheel(clock_generator, pcu_->GetPowerPort(2), 1, file_name, global_environment_->GetSimulationTime().GetAttitudeUpdateInterval_s(),
                                           global_environment_->GetSimulationTime().GetComponentStepTime_s()));
 
   // Torque Generator
