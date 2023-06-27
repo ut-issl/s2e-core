3b7b2051b8318760b75fe9559acd47ee946be9ab
Satoshi Ikari <ikari@space.t.u-tokyo.ac.jp>
2023-06-27 16:35:53 +0200

    Add comments

--- src/library/atmosphere/harris_priester_model.hpp
+++ src/library/atmosphere/harris_priester_model.hpp
@@ -1,6 +1,7 @@
 /**
  * @file harris_priester_model.hpp
  * @brief Harris-Priester earth's atmospheric density model
+ * @note Ref: O. Montenbruck and E. Gill, Satellite Orbits, Chp. 3.5
  */
 #ifndef S2E_LIBRARY_HARRIS_PRIESTER_MODEL_HPP_
 #define S2E_LIBRARY_HARRIS_PRIESTER_MODEL_HPP_
@@ -13,10 +14,12 @@ namespace libra::atmosphere {
 /**
  * @fn CalcAirDensityWithHarrisPriester
  * @brief Calculate atmospheric density with Harris-Priester method
- * @param [in]
+ * @param [in] geodetic_position: Spacecraft geodetic position
+ * @param [in] sun_direction_eci: Sun direction unit vector in ECI frame
+ * @param [in] exponent_parameter: n in the equation. n=2 for low inclination orbit and n=6 for polar orbit.
  * @return Atmospheric density [kg/m^3]
  */
-double CalcAirDensityWithHarrisPriester_kg_m3(const GeodeticPosition geodetic_position, const libra::Vector<3> sun_direction_i,
+double CalcAirDensityWithHarrisPriester_kg_m3(const GeodeticPosition geodetic_position, const libra::Vector<3> sun_direction_eci,
                                               const double exponent_parameter = 4);
 
 }  // namespace libra::atmosphere
