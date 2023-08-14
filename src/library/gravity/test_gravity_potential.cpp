22e7e4a367d9ac8566f7d74caaf9b5f6f301aa76
Satoshi Ikari <ikari@space.t.u-tokyo.ac.jp>
2023-08-14 14:28:38 +0200

    Fix format

--- src/library/gravity/test_gravity_potential.cpp
+++ src/library/gravity/test_gravity_potential.cpp
@@ -78,27 +78,23 @@ TEST(GravityPotential, PartialDerivative1) {
   // Calc Acceleration and numerical partial derivatives
   double d_r = 1e-9;
   libra::Matrix<3, 3> numerical_partial_derivative_xcxf_s2;
-  for (size_t i = 0; i < 3; i++)
-  {
+  for (size_t i = 0; i < 3; i++) {
     libra::Vector<3> position_1_xcxf_m = position_xcxf_m;
     libra::Vector<3> position_2_xcxf_m = position_xcxf_m;
-    position_1_xcxf_m[i] = position_xcxf_m[i] - d_r/2.0;
-    position_2_xcxf_m[i] = position_xcxf_m[i] + d_r/2.0;
+    position_1_xcxf_m[i] = position_xcxf_m[i] - d_r / 2.0;
+    position_2_xcxf_m[i] = position_xcxf_m[i] + d_r / 2.0;
     libra::Vector<3> acceleration_1_xcxf_m_s2 = gravity_potential_.CalcAcceleration_xcxf_m_s2(position_1_xcxf_m);
     libra::Vector<3> acceleration_2_xcxf_m_s2 = gravity_potential_.CalcAcceleration_xcxf_m_s2(position_2_xcxf_m);
-    libra::Vector<3> diff_acceleration_xcxf_m_s2 =  acceleration_2_xcxf_m_s2 - acceleration_1_xcxf_m_s2;
-    for (size_t j = 0; j < 3; j++)
-    {
+    libra::Vector<3> diff_acceleration_xcxf_m_s2 = acceleration_2_xcxf_m_s2 - acceleration_1_xcxf_m_s2;
+    for (size_t j = 0; j < 3; j++) {
       numerical_partial_derivative_xcxf_s2[i][j] = diff_acceleration_xcxf_m_s2[j] / d_r;
     }
   }
 
   // Compare numerical and analytical calculation
   libra::Matrix<3, 3> diff;
-  for (size_t i = 0; i < 3; i++)
-  {
-    for (size_t j = 0; j < 3; j++)
-    {
+  for (size_t i = 0; i < 3; i++) {
+    for (size_t j = 0; j < 3; j++) {
       EXPECT_NEAR(numerical_partial_derivative_xcxf_s2[i][j], partial_derivative_xcxf_s2[i][j], accuracy);
       diff[i][j] = numerical_partial_derivative_xcxf_s2[i][j] - partial_derivative_xcxf_s2[i][j];
     }
@@ -135,27 +131,23 @@ TEST(GravityPotential, PartialDerivative2) {
   // Calc Acceleration and numerical partial derivatives
   double d_r = 1e-9;
   libra::Matrix<3, 3> numerical_partial_derivative_xcxf_s2;
-  for (size_t i = 0; i < 3; i++)
-  {
+  for (size_t i = 0; i < 3; i++) {
     libra::Vector<3> position_1_xcxf_m = position_xcxf_m;
     libra::Vector<3> position_2_xcxf_m = position_xcxf_m;
-    position_1_xcxf_m[i] = position_xcxf_m[i] - d_r/2.0;
-    position_2_xcxf_m[i] = position_xcxf_m[i] + d_r/2.0;
+    position_1_xcxf_m[i] = position_xcxf_m[i] - d_r / 2.0;
+    position_2_xcxf_m[i] = position_xcxf_m[i] + d_r / 2.0;
     libra::Vector<3> acceleration_1_xcxf_m_s2 = gravity_potential_.CalcAcceleration_xcxf_m_s2(position_1_xcxf_m);
     libra::Vector<3> acceleration_2_xcxf_m_s2 = gravity_potential_.CalcAcceleration_xcxf_m_s2(position_2_xcxf_m);
-    libra::Vector<3> diff_acceleration_xcxf_m_s2 =  acceleration_2_xcxf_m_s2 - acceleration_1_xcxf_m_s2;
-    for (size_t j = 0; j < 3; j++)
-    {
+    libra::Vector<3> diff_acceleration_xcxf_m_s2 = acceleration_2_xcxf_m_s2 - acceleration_1_xcxf_m_s2;
+    for (size_t j = 0; j < 3; j++) {
       numerical_partial_derivative_xcxf_s2[i][j] = diff_acceleration_xcxf_m_s2[j] / d_r;
     }
   }
 
   // Compare numerical and analytical calculation
   libra::Matrix<3, 3> diff;
-  for (size_t i = 0; i < 3; i++)
-  {
-    for (size_t j = 0; j < 3; j++)
-    {
+  for (size_t i = 0; i < 3; i++) {
+    for (size_t j = 0; j < 3; j++) {
       EXPECT_NEAR(numerical_partial_derivative_xcxf_s2[i][j], partial_derivative_xcxf_s2[i][j], accuracy);
       diff[i][j] = numerical_partial_derivative_xcxf_s2[i][j] - partial_derivative_xcxf_s2[i][j];
     }
