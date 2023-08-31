aa62d002ce3f9f47b3109a91d932805aad2592d7
Satoshi Ikari <ikari@space.t.u-tokyo.ac.jp>
2023-08-31 16:02:54 +0200

    Add WingsOperationFile class

---
+++ src/library/initialize/wings_operation_file.cpp
@@ -0,0 +1,7 @@
+/**
+ * @file wings_operation_file.cpp
+ * @brief Classes and functions to read and manage WINGS's operation file (.ops)
+ */
+
+#include "wings_operation_file.hpp"
+
--- src/library/initialize/wings_operation_file.hpp
+++ src/library/initialize/wings_operation_file.hpp
@@ -3,3 +3,26 @@
  * @brief Classes and functions to read and manage WINGS's operation file (.ops)
  */
 
+#ifndef S2E_LIBRARY_INITIALIZE_WINGS_OPERATION_FILE_HPP_
+#define S2E_LIBRARY_INITIALIZE_WINGS_OPERATION_FILE_HPP_
+
+#include <string>
+
+/**
+ * @class WingsOperationFile
+ * @brief A class to handle WINGS operation file
+ */
+class WingsOperationFile {
+ public:
+  /**
+   * @fn WingsOperationFile
+   * @brief Constructor
+   * @param[in] file_path: File path of the operation file
+   */
+  WingsOperationFile(const std::string file_path);
+
+ private:
+
+};
+
+#endif  // S2E_LIBRARY_INITIALIZE_WINGS_OPERATION_FILE_HPP_
