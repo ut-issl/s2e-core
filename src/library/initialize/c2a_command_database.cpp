db4b6a9d95141c295cf1c60e3c83ac2cccd1e986
Satoshi Ikari <ikari@space.t.u-tokyo.ac.jp>
2023-09-04 19:48:42 +0200

    Fix to use S2E's endian_memcpy

--- src/library/initialize/c2a_command_database.cpp
+++ src/library/initialize/c2a_command_database.cpp
@@ -8,19 +8,8 @@
 #include <fstream>
 #include <iostream>
 #include <library/utilities/macros.hpp>
+#include <library/utilities/endian.hpp>
 #include <sstream>
-
-#ifdef USE_C2A
-#if C2A_CORE_VER_MAJOR == 4
-// c2a-core v4
-#include "src_core/library/endian.h"
-#elif C2A_CORE_VER_MAJOR <= 3
-// c2a-core <= v3
-#include "src_core/Library/endian.h"
-#else
-#error "c2a-core version is not supported"
-#endif  // c2a-core version header
-#endif  // USE_C2A
 
 C2aCommandInformation::C2aCommandInformation(const std::string cmd_db_line) {
   if (cmd_db_line.find("*") == 0) return;
@@ -99,66 +88,65 @@ C2aCommandDatabase::C2aCommandDatabase(const std::string file_path) {
 }
 
 void DecodeC2aCommandArgument(const C2aArgumentType type, const std::string argument_string, uint8_t* param, size_t& size_param) {
-#ifdef USE_C2A
   switch (type) {
     case C2aArgumentType::kUint8t: {
       size_param = 1;
       uint8_t argument = (uint8_t)std::stoul(argument_string);  // TODO: 範囲外処理
-      ENDIAN_memcpy(param, &argument, size_param);
+      endian_memcpy(param, &argument, size_param);
       break;
     }
     case C2aArgumentType::kUint16t: {
       size_param = 2;
       uint16_t argument = (uint16_t)std::stoul(argument_string);  // TODO: 範囲外処理
-      ENDIAN_memcpy(param, &argument, size_param);
+      endian_memcpy(param, &argument, size_param);
       break;
     }
     case C2aArgumentType::kUint32t: {
       size_param = 4;
       uint32_t argument = (uint32_t)std::stoul(argument_string);  // TODO: 範囲外処理
-      ENDIAN_memcpy(param, &argument, size_param);
+      endian_memcpy(param, &argument, size_param);
       break;
     }
     case C2aArgumentType::kUint64t: {
       size_param = 8;
       uint64_t argument = std::stoul(argument_string);
-      ENDIAN_memcpy(param, &argument, size_param);
+      endian_memcpy(param, &argument, size_param);
       break;
     }
     case C2aArgumentType::kInt8t: {
       size_param = 1;
       int8_t argument = (int8_t)std::stoi(argument_string);  // TODO: 範囲外処理
-      ENDIAN_memcpy(param, &argument, size_param);
+      endian_memcpy(param, &argument, size_param);
       break;
     }
     case C2aArgumentType::kInt16t: {
       size_param = 2;
       int16_t argument = (int16_t)std::stoi(argument_string);  // TODO: 範囲外処理
-      ENDIAN_memcpy(param, &argument, size_param);
+      endian_memcpy(param, &argument, size_param);
       break;
     }
     case C2aArgumentType::kInt32t: {
       size_param = 4;
       int32_t argument = std::stoi(argument_string);
-      ENDIAN_memcpy(param, &argument, size_param);
+      endian_memcpy(param, &argument, size_param);
       break;
     }
     case C2aArgumentType::kInt64t: {
       size_param = 8;
       int64_t argument = std::stol(argument_string);
-      ENDIAN_memcpy(param, &argument, size_param);
+      endian_memcpy(param, &argument, size_param);
       break;
     }
     case C2aArgumentType::kFloat: {
       size_param = 4;
       float argument = std::stof(argument_string);
-      ENDIAN_memcpy(param, &argument, size_param);
+      endian_memcpy(param, &argument, size_param);
       break;
     }
     case C2aArgumentType::kDouble: {
       size_param = 8;
       double argument = std::stod(argument_string);
-      ENDIAN_memcpy(param, &argument, size_param);
+      endian_memcpy(param, &argument, size_param);
       break;
     }
     case C2aArgumentType::kRaw: {
@@ -168,9 +156,4 @@ void DecodeC2aCommandArgument(const C2aArgumentType type, const std::string argu
     default:
       break;
   }
-#else
-  UNUSED(argument_string);
-  UNUSED(param);
-  UNUSED(size_param);
-#endif
 }
