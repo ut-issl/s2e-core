c378c78c31bce9d70d7be7fea468b26e37713900
Satoshi Ikari <ikari@space.t.u-tokyo.ac.jp>
2023-09-26 09:52:42 +0200

    Move initialize function for magnetorquer

--- src/components/real/aocs/magnetorquer.hpp
+++ src/components/real/aocs/magnetorquer.hpp
@@ -146,4 +146,28 @@ class Magnetorquer : public Component, public ILoggable {
   libra::Vector<kMtqDimension> CalcOutputTorque(void);
 };
 
+/**
+ * @fn InitMagnetorquer
+ * @brief Initialize functions for magnetometer without power port
+ * @param [in] clock_generator: Clock generator
+ * @param [in] actuator_id: Actuator ID
+ * @param [in] file_name: Path to the initialize file
+ * @param [in] component_step_time_s: Component step time [sec]
+ * @param [in] geomagnetic_field: Geomegnetic environment
+ */
+Magnetorquer InitMagnetorquer(ClockGenerator* clock_generator, int actuator_id, const std::string file_name, double component_step_time_s,
+                              const GeomagneticField* geomagnetic_field);
+/**
+ * @fn InitMagnetorquer
+ * @brief Initialize functions for magnetometer with power port
+ * @param [in] clock_generator: Clock generator
+ * @param [in] power_port: Power port
+ * @param [in] actuator_id: Actuator ID
+ * @param [in] file_name: Path to the initialize file
+ * @param [in] component_step_time_s: Component step time [sec]
+ * @param [in] geomagnetic_field: Geomegnetic environment
+ */
+Magnetorquer InitMagnetorquer(ClockGenerator* clock_generator, PowerPort* power_port, int actuator_id, const std::string file_name,
+                              double component_step_time_s, const GeomagneticField* geomagnetic_field);
+
 #endif  // S2E_COMPONENTS_REAL_AOCS_MAGNETORQUER_HPP_
