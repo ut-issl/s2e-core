64cb0b70eb6759bb69dae38410aa6f13058d5cf8
Satoshi Ikari <ikari@space.t.u-tokyo.ac.jp>
2023-10-06 09:47:19 +0200

    Remove CMD_DB header info

--- src/library/initialize/c2a_command_database.cpp
+++ src/library/initialize/c2a_command_database.cpp
@@ -13,6 +13,7 @@
 
 C2aCommandInformation::C2aCommandInformation(const std::string cmd_db_line) {
   if (cmd_db_line.find("*") == 0) return;
+  if (cmd_db_line.find(",") != 0) return;
 
   // CSV
   std::string token;
