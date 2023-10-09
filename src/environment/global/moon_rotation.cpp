0ca4782885a4aee6fba6640cc645db5a2c31c5b1
Satoshi Ikari <ikari@space.t.u-tokyo.ac.jp>
2023-10-09 15:29:33 +0200

    Implemente moon rotation

---
+++ src/environment/global/moon_rotation.cpp
@@ -0,0 +1,48 @@
+﻿/**
+ * @file moon_rotation.cpp
+ * @brief Class to calculate the moon rotation
+ * @note Ref: A Standardized Lunar Coordinate System for the Lunar Reconnaissance Orbiter and Lunar Datasets
+ *            https://lunar.gsfc.nasa.gov/library/LunCoordWhitePaper-10-08.pdf
+ *            https://naif.jpl.nasa.gov/pub/naif/generic_kernels/spk/planets/de430_moon_coord.pdf
+ */
+
+#include "moon_rotation.hpp"
+
+#include <library/math/constants.hpp>
+
+MoonRotation::MoonRotation() { dcm_me_pa_ = CalcDcmMeanEarthToPrincipalAxis(); }
+
+void MoonRotation::Update(const libra::Vector<3> moon_position_eci_m, const libra::Vector<3> moon_velocity_eci_m_s) {
+  libra::Matrix<3, 3> dcm_eci_to_me = CalcDcmEciToMeanEarth(moon_position_eci_m, moon_velocity_eci_m_s);
+  libra::Matrix<3, 3> dcm_eci_to_pa = dcm_me_pa_ * dcm_eci_to_me;
+  dcm_j2000_to_mcmf_ = dcm_eci_to_pa;
+}
+
+libra::Matrix<3, 3> MoonRotation::CalcDcmEciToMeanEarth(const libra::Vector<3> moon_position_eci_m, const libra::Vector<3> moon_velocity_eci_m_s) {
+  libra::Vector<3> me_ex_eci = -1.0 * moon_position_eci_m.CalcNormalizedVector();
+
+  libra::Vector<3> moon_orbit_norm = libra::OuterProduct(moon_position_eci_m, moon_velocity_eci_m_s);
+  libra::Vector<3> me_ez_eci = moon_orbit_norm.CalcNormalizedVector();
+
+  libra::Vector<3> me_ey_eci = libra::OuterProduct(me_ez_eci, me_ex_eci);
+
+  libra::Matrix<3, 3> dcm_eci_to_me;
+  for (size_t i; i < 3; i++) {
+    dcm_eci_to_me[0][i] = me_ex_eci[i];
+    dcm_eci_to_me[1][i] = me_ey_eci[i];
+    dcm_eci_to_me[2][i] = me_ez_eci[i];
+  }
+
+  return dcm_eci_to_me;
+}
+
+libra::Matrix<3, 3> MoonRotation::CalcDcmMeanEarthToPrincipalAxis() {
+  const double theta_x_rad = 0.285 * libra::arcsec_to_rad;
+  const double theta_y_rad = 78.580 * libra::arcsec_to_rad;
+  const double theta_z_rad = 67.573 * libra::arcsec_to_rad;
+
+  libra::Matrix<3, 3> dcm_me_pa =
+      libra::MakeRotationMatrixZ(theta_z_rad) * libra::MakeRotationMatrixY(theta_y_rad) * libra::MakeRotationMatrixX(theta_x_rad);
+
+  return dcm_me_pa;
+}
--- src/environment/global/moon_rotation.hpp
+++ src/environment/global/moon_rotation.hpp
@@ -1,12 +1,16 @@
 ﻿/**
  * @file moon_rotation.hpp
  * @brief Class to calculate the moon rotation
+ * @note Ref: A Standardized Lunar Coordinate System for the Lunar Reconnaissance Orbiter and Lunar Datasets
+ *            https://lunar.gsfc.nasa.gov/library/LunCoordWhitePaper-10-08.pdf
+ *            https://naif.jpl.nasa.gov/pub/naif/generic_kernels/spk/planets/de430_moon_coord.pdf
  */
 
 #ifndef S2E_ENVIRONMENT_GLOBAL_MOON_ROTATION_HPP_
 #define S2E_ENVIRONMENT_GLOBAL_MOON_ROTATION_HPP_
 
 #include "library/math/matrix.hpp"
+#include "library/math/vector.hpp"
 
 /**
  * @class MoonRotation
@@ -17,27 +21,39 @@ class MoonRotation {
   /**
    * @fn MoonRotation
    * @brief Constructor
-   * @param [in] rotation_mode: Designation of rotation model
    */
-  MoonRotation(const MoonRotationMode rotation_mode = MoonRotationMode::kSimple);
+  MoonRotation();
 
   /**
    * @fn Update
    * @brief Update rotation
    * @param [in] julian_date: Julian date
    */
-  void Update(const double julian_date);
+  void Update(const libra::Vector<3> moon_position_eci_m, const libra::Vector<3> moon_velocity_eci_m_s);
 
   /**
-   * @fn GetDcmJ2000ToXcxf
-   * @brief Return the DCM between J2000 inertial frame and the frame of fixed to the target object X (X-Centered X-Fixed)
+   * @fn GetDcmJ2000ToMcmf
+   * @brief Return the DCM between J2000 inertial frame and the Moon Centered Moon Fixed frame
+   * @note Because this is just a DCM, users need to consider the origin of the vector, which you want to convert with this matrix.
    */
-  inline const libra::Matrix<3, 3> GetDcmJ2000ToXcxf() const { return dcm_j2000_to_xcxf_; };
+  inline const libra::Matrix<3, 3> GetDcmJ2000ToMcmf() const { return dcm_j2000_to_mcmf_; };
 
  private:
-  libra::Matrix<3, 3> dcm_j2000_to_xcxf_;  //!< Direction Cosine Matrix J2000 to XCXF(X-Centered X-Fixed)
+  libra::Matrix<3, 3> dcm_j2000_to_mcmf_;  //!< Direction Cosine Matrix J2000 to MCMF
+  libra::Matrix<3, 3> dcm_me_pa_;          //!< DCM from ME (Mean Earth) moon fixed frame to PA (Principal Axis) moon fixed frame
 
-  MoonRotationMode rotation_mode_;  //!< Designation of dynamics model
+  /**
+   * @fn CalcDcmEciToMe
+   * @brief Calculate DCM from ECI to ME (Mean Earth) moon fixed frame
+   * @param[in] moon_position_eci_m: Moon position vector @ ECI frame [m]
+   * @param[in] moon_velocity_eci_m_s: Moon velocity vector @ ECI frame [m/s]
+   */
+  libra::Matrix<3, 3> CalcDcmEciToMeanEarth(const libra::Vector<3> moon_position_eci_m, const libra::Vector<3> moon_velocity_eci_m_s);
+  /**
+   * @fn CalcDcmMeToPa
+   * @brief Calculate DCM from ME (Mean Earth) moon fixed frame to PA (Principal Axis) moon fixed frame
+   */
+  libra::Matrix<3, 3> CalcDcmMeanEarthToPrincipalAxis();
 };
 
 #endif  // S2E_ENVIRONMENT_GLOBAL_MOON_ROTATION_HPP_
