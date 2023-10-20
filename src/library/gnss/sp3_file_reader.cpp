f97168a49b2db16a3e6dc278d4e6bd9d37f37503
Satoshi Ikari <ikari@space.t.u-tokyo.ac.jp>
2023-10-20 15:31:46 +0200

    Add SP3 file reader in library

---
+++ src/library/gnss/sp3_file_reader.cpp
@@ -0,0 +1,12 @@
+/**
+ * @file sp3_file_reader.cpp
+ * @brief A class to read the SP3 (Extended Standard Product 3) format file and provide functions to access the data
+ * @note Support version: SP3-d
+ *       Ref: https://files.igs.org/pub/data/format/sp3d.pdf?_ga=2.115202830.823990648.1664976786-1792483726.1664976785
+ */
+
+#include "sp3_file_reader.hpp"
+
+#include <iostream>
+
+Sp3FileReader::Sp3FileReader() {}
---
+++ src/library/gnss/sp3_file_reader.hpp
@@ -0,0 +1,30 @@
+/**
+ * @file sp3_file_reader.hpp
+ * @brief A class to read the SP3 (Extended Standard Product 3) format file and provide functions to access the data
+ * @note Support version: SP3-d
+ *       Ref: https://files.igs.org/pub/data/format/sp3d.pdf?_ga=2.115202830.823990648.1664976786-1792483726.1664976785
+ */
+
+#ifndef S2E_LIBRARY_GNSS_SP3_FILE_READER_HPP_
+#define S2E_LIBRARY_GNSS_SP3_FILE_READER_HPP_
+
+#include <library/math/vector.hpp>
+#include <string>
+#include <vector>
+
+/**
+ * @class Sp3FileReader
+ * @brief
+ */
+class Sp3FileReader {
+ public:
+  /**
+   * @fn Sp3FileReader
+   * @brief Constructor
+   */
+  Sp3FileReader();
+
+ private:
+};
+
+#endif  // S2E_LIBRARY_GNSS_SP3_FILE_READER_HPP_
