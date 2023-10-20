6bc0253cee68f0bc9e59336d7f99ea4aaffb4a1a
Satoshi Ikari <ikari@space.t.u-tokyo.ac.jp>
2023-10-20 21:39:38 +0200

    Add date time format library

---
+++ src/library/time_system/date_time_format.cpp
@@ -0,0 +1,22 @@
+/**
+ * @file date_time_format.cpp
+ * @brief
+ */
+
+#include "date_time_format.hpp"
+
+#include <cmath>
+#include <sstream>
+
+std::string DateTime::GetAsString() const {
+  std::stringstream stream;
+  stream << std::setw(4) << std::setfill('0') << (int)year_ << "/";
+  stream << std::setw(2) << std::setfill('0') << (int)month_ << "/";
+  stream << std::setw(2) << std::setfill('0') << (int)day_ << " ";
+
+  stream << std::setw(2) << std::setfill('0') << (int)hour_ << ":";
+  stream << std::setw(2) << std::setfill('0') << (int)minute_ << ":";
+  stream << second_;
+  std::string output = stream.str();
+  return output;
+}
