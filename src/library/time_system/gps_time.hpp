43b0a115f255e3a0aadedd00e24628513860cf6d
Satoshi Ikari <ikari@space.t.u-tokyo.ac.jp>
2023-10-21 00:10:44 +0200

    Fix variable name

--- src/library/time_system/gps_time.hpp
+++ src/library/time_system/gps_time.hpp
@@ -19,7 +19,7 @@ class GpsTime {
    * @fn GpsTime
    * @brief Constructor initialized with week and second
    */
-  GpsTime(const size_t week = 0, const double time_of_week = 0.0) : week_(week), time_of_week_(time_of_week) {}
+  GpsTime(const size_t week = 0, const double elapsed_time_from_week_s = 0.0) : week_(week), elapsed_time_from_week_s_(elapsed_time_from_week_s) {}
 
   ~GpsTime() {}
 
@@ -32,11 +32,11 @@ class GpsTime {
    * @fn GetTime
    * @return GPS time
    */
-  inline double GetTimeOfWeek() const { return time_of_week_; }
+  inline double GetElapsedTimeFromWeek_s() const { return elapsed_time_from_week_s_; }
 
  private:
-  size_t week_;          //!< GPS week (week = 0 at 6th Jan. 1980)
-  double time_of_week_;  //!< Time of week [0,0, 604800.0)
+  size_t week_;                      //!< GPS week (week = 0 at 6th Jan. 1980)
+  double elapsed_time_from_week_s_;  //!< Elapsed time from the GPS week [s] [0,0, 604800.0)
 };
 
 #endif  // S2E_LIBRARY_TIME_SYSTEM_GPS_TIME_HPP_
