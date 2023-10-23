5cac20d249e975ba415fecef87e9a9172306e185
Satoshi Ikari <ikari@space.t.u-tokyo.ac.jp>
2023-10-23 09:42:48 +0200

    Add test for GpsTime

---
+++ src/library/time_system/test_gps_time.cpp
@@ -0,0 +1,10 @@
+#include <gtest/gtest.h>
+
+#include "gps_time.hpp"
+
+TEST(GpsTime, ConstructorNominal) {
+  GpsTime gps_time(2285, 12345.6798);
+
+  EXPECT_EQ(2285, gps_time.GetWeek());
+  EXPECT_DOUBLE_EQ(12345.6798, gps_time.GetElapsedTimeFromWeek_s());
+}
