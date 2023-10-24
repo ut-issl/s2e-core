5ef6c405aa4b845d55233b3d5d480071db399acb
Satoshi Ikari <ikari@space.t.u-tokyo.ac.jp>
2023-10-24 14:18:56 +0200

    Add test for quadratic function

--- src/library/math/test_interpolation.cpp
+++ src/library/math/test_interpolation.cpp
@@ -21,3 +21,19 @@ TEST(Interpolation, LinearFunction) {
   xx = 3.6;
   EXPECT_DOUBLE_EQ(2.0*xx, interpolation.CalcPolynomial(xx));
 }
+
+/**
+ * @brief Test for quadratic function
+ */
+TEST(Interpolation, QuadraticFunction) {
+  std::vector<double> x{0.0, 1.0, 2.0, 3.0, 4.0};
+  std::vector<double> y{0.0, 1.0, 4.0, 9.0, 16.0};
+  libra::Interpolation interpolation(x, y);
+
+  double xx = 0.4;
+  EXPECT_DOUBLE_EQ(pow(xx, 2.0), interpolation.CalcPolynomial(xx));
+  xx = 2.4;
+  EXPECT_DOUBLE_EQ(pow(xx, 2.0), interpolation.CalcPolynomial(xx));
+  xx = 3.8;
+  EXPECT_DOUBLE_EQ(pow(xx, 2.0), interpolation.CalcPolynomial(xx));
+}
