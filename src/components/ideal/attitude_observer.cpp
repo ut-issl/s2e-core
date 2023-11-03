cb0df6b45081bfa8f0b560439de7caf3913f946a
Satoshi Ikari <ikari@space.t.u-tokyo.ac.jp>
2023-11-03 15:43:46 +0100

    Add attitude observer

---
+++ src/components/ideal/attitude_observer.cpp
@@ -0,0 +1,64 @@
+/*
+ * @file attitude_observer.cpp
+ * @brief Ideal component which can observe attitude
+ */
+
+#include "attitude_observer.hpp"
+
+#include <library/initialize/initialize_file_access.hpp>
+#include <library/math/constants.hpp>
+
+AttitudeObserver::AttitudeObserver(const int prescaler, ClockGenerator* clock_generator, const double standard_deviation_rad,
+                                   const Attitude* attitude)
+    : Component(prescaler, clock_generator), angle_noise_(0.0, standard_deviation_rad), attitude_(attitude) {
+  direction_noise_.SetParameters(0.0, 1.0);
+}
+
+void AttitudeObserver::MainRoutine(const int time_count) {
+  UNUSED(time_count);
+  
+  // Error calculation
+  libra::Vector<3> random_direction;
+  random_direction[0] = direction_noise_;
+  random_direction[1] = direction_noise_;
+  random_direction[2] = direction_noise_;
+  random_direction = random_direction.CalcNormalizedVector();
+
+  double error_angle_rad = angle_noise_;
+  libra::Quaternion error_quaternion(random_direction, error_angle_rad);
+
+  measured_quaternion_i2b_ = error_quaternion * attitude_->GetQuaternion_i2b();
+}
+
+std::string AttitudeObserver::GetLogHeader() const {
+  std::string str_tmp = "";
+
+  std::string head = "attitude_observer_";
+  str_tmp += WriteQuaternion(head + "quaternion", "i2b");
+
+  return str_tmp;
+}
+
+std::string AttitudeObserver::GetLogValue() const {
+  std::string str_tmp = "";
+
+  str_tmp += WriteQuaternion(measured_quaternion_i2b_);
+
+  return str_tmp;
+}
+
+AttitudeObserver InitializeAttitudeObserver(ClockGenerator* clock_generator, const std::string file_name, const Attitude* attitude) {
+  // General
+  IniAccess ini_file(file_name);
+
+  // CompoBase
+  int prescaler = ini_file.ReadInt("COMPONENT_BASE", "prescaler");
+  if (prescaler <= 1) prescaler = 1;
+
+  // ForceGenerator
+  double error_angle_standard_deviation_deg = ini_file.ReadDouble("ATTITUDE_OBSERVER", "error_angle_standard_deviation_deg");
+  double error_angle_standard_deviation_rad = libra::deg_to_rad * error_angle_standard_deviation_deg;
+  AttitudeObserver attitude_observer(prescaler, clock_generator, error_angle_standard_deviation_rad, attitude);
+
+  return attitude_observer;
+}
---
+++ src/components/ideal/attitude_observer.hpp
@@ -0,0 +1,81 @@
+/*
+ * @file attitude_observer.hpp
+ * @brief Ideal component which can observe attitude
+ */
+
+#ifndef S2E_COMPONENTS_IDEAL_ATTITUDE_OBSERVER_HPP_
+#define S2E_COMPONENTS_IDEAL_ATTITUDE_OBSERVER_HPP_
+
+#include <dynamics/attitude/attitude.hpp>
+#include <library/logger/loggable.hpp>
+#include <library/math/quaternion.hpp>
+#include <library/randomization/normal_randomization.hpp>
+
+#include "../base/component.hpp"
+
+/*
+ * @class AttitudeObserver
+ * @brief Class to emulate star tracker
+ */
+class AttitudeObserver : public Component, public ILoggable {
+ public:
+  /**
+   * @fn AttitudeObserver
+   * @brief Constructor without power port
+   * @param [in] prescaler: Frequency scale factor for update
+   * @param [in] clock_generator: Clock generator
+   * @param [in] attitude: Attitude information
+   */
+  AttitudeObserver(const int prescaler, ClockGenerator* clock_generator, const double standard_deviation_rad, const Attitude* attitude);
+
+  /**
+   * @fn ~AttitudeObserver
+   * @brief Destructor
+   */
+  ~AttitudeObserver() {}
+
+  // Override functions for Component
+  /**
+   * @fn MainRoutine
+   * @brief Main routine for sensor observation
+   */
+  void MainRoutine(const int time_count) override;
+
+  // Override ILoggable
+  /**
+   * @fn GetLogHeader
+   * @brief Override GetLogHeader function of ILoggable
+   */
+  virtual std::string GetLogHeader() const override;
+  /**
+   * @fn GetLogValue
+   * @brief Override GetLogValue function of ILoggable
+   */
+  virtual std::string GetLogValue() const override;
+
+  /**
+   * @fn GetQuaternion_i2c
+   * @brief Return observed quaternion from the inertial frame to the component frame
+   */
+  inline const libra::Quaternion GetQuaternion_i2b() const { return measured_quaternion_i2b_; };
+
+ protected:
+  libra::Quaternion measured_quaternion_i2b_ = {0.0, 0.0, 0.0, 1.0};  //!< StarSensor observed quaternion
+
+  libra::NormalRand angle_noise_;      //!< Normal random for magnitude noise
+  libra::NormalRand direction_noise_;  //!< Normal random for direction noise
+
+  // Observed variables
+  const Attitude* attitude_;  //!< Attitude information
+};
+
+/**
+ * @fn InitStarSensor
+ * @brief Initialize functions for StarSensor without power port
+ * @param [in] clock_generator: Clock generator
+ * @param [in] file_name: Path to the initialize file
+ * @param [in] attitude: Attitude information
+ */
+AttitudeObserver InitializeAttitudeObserver(ClockGenerator* clock_generator, const std::string file_name, const Attitude* attitude);
+
+#endif  // S2E_COMPONENTS_IDEAL_ATTITUDE_OBSERVER_HPP_
