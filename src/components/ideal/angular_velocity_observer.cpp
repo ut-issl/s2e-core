306a18c44693b8b0f2465ab6bf3edb0f384e6aff
Satoshi Ikari <ikari@space.t.u-tokyo.ac.jp>
2023-11-03 14:00:08 +0100

    Add angular velocity observer

---
+++ src/components/ideal/angular_velocity_observer.cpp
@@ -0,0 +1,49 @@
+/*
+ * @file angular_velocity_observer.cpp
+ * @brief Ideal component which can observe angular velocity
+ */
+
+#include "angular_velocity_observer.hpp"
+
+#include <library/initialize/initialize_file_access.hpp>
+
+AngularVelocityObserver::AngularVelocityObserver(const int prescaler, ClockGenerator* clock_generator, Sensor& sensor_base, const Dynamics* dynamics)
+    : Component(prescaler, clock_generator), Sensor(sensor_base), dynamics_(dynamics) {}
+
+void AngularVelocityObserver::MainRoutine(const int time_count) {
+  UNUSED(time_count);
+  angular_velocity_b_rad_s_ = Measure(dynamics_->GetAttitude().GetAngularVelocity_b_rad_s());  // Add noises
+}
+
+std::string AngularVelocityObserver::GetLogHeader() const {
+  std::string str_tmp = "";
+
+  std::string sensor_name = "angular_velocity_observer_";
+  str_tmp += WriteVector(sensor_name + "measured_angular_velocity", "b", "rad/s", 3);
+
+  return str_tmp;
+}
+
+std::string AngularVelocityObserver::GetLogValue() const {
+  std::string str_tmp = "";
+
+  str_tmp += WriteVector(angular_velocity_b_rad_s_);
+
+  return str_tmp;
+}
+
+AngularVelocityObserver InitAngularVelocityObserver(ClockGenerator* clock_generator, const std::string file_name, double component_step_time_s,
+                                                    const Dynamics* dynamics) {
+  IniAccess ini_file(file_name);
+  const std::string section_name = "ANGULAR_VELOCITY_OBSERVER";
+
+  int prescaler = ini_file.ReadInt(section_name.c_str(), "prescaler");
+  if (prescaler <= 1) prescaler = 1;
+
+  // Sensor
+  Sensor<3> sensor_base = ReadSensorInformation<3>(file_name, component_step_time_s * (double)(prescaler), section_name.c_str(), "rad_s");
+
+  AngularVelocityObserver observer(prescaler, clock_generator, sensor_base, dynamics);
+
+  return observer;
+}
---
+++ src/components/ideal/angular_velocity_observer.hpp
@@ -0,0 +1,76 @@
+/*
+ * @file angular_velocity_observer.hpp
+ * @brief Ideal component which can observe angular velocity
+ */
+
+#ifndef S2E_COMPONENTS_IDEAL_ANGULAR_VELOCITY_OBSERVER_HPP_
+#define S2E_COMPONENTS_IDEAL_ANGULAR_VELOCITY_OBSERVER_HPP_
+
+#include <dynamics/dynamics.hpp>
+#include <library/logger/loggable.hpp>
+
+#include "../base/component.hpp"
+#include "../base/sensor.hpp"
+
+/*
+ * @class AngularVelocityObserver
+ * @brief Ideal component which can observe angular velocity
+ */
+class AngularVelocityObserver : public Component, public Sensor<3>, public ILoggable {
+ public:
+  /**
+   * @fn AngularVelocityObserver
+   * @brief Constructor
+   * @param [in] prescaler: Frequency scale factor for update
+   * @param [in] clock_generator: Clock generator
+   * @param [in] sensor_base: Sensor base information
+   * @param [in] dynamics: Dynamics information
+   */
+  AngularVelocityObserver(const int prescaler, ClockGenerator* clock_generator, Sensor& sensor_base, const Dynamics* dynamics);
+  /**
+   * @fn ~AngularVelocityObserver
+   * @brief Destructor
+   */
+  ~AngularVelocityObserver() {}
+
+  // Override functions for Component
+  /**
+   * @fn MainRoutine
+   * @brief Main routine to calculate force generation
+   */
+  void MainRoutine(const int time_count) override;
+
+  // Override ILoggable
+  /**
+   * @fn GetLogHeader
+   * @brief Override GetLogHeader function of ILoggable
+   */
+  virtual std::string GetLogHeader() const override;
+  /**
+   * @fn GetLogValue
+   * @brief Override GetLogValue function of ILoggable
+   */
+  virtual std::string GetLogValue() const override;
+
+  // Getter
+
+  // Setter
+
+ protected:
+  libra::Vector<3> angular_velocity_b_rad_s_{0.0};  //!< Observed angular velocity [rad/s]
+
+  const Dynamics* dynamics_;  //!< Dynamics information
+};
+
+/**
+ * @fn InitializeAngularVelocityObserver
+ * @brief Initialize function for AngularVelocityObserver
+ * @param [in] clock_generator: Clock generator
+ * @param [in] file_name: Path to the initialize file
+ * @param [in] component_step_time_s: Component step time [sec]
+ * @param [in] dynamics: Dynamics information
+ */
+AngularVelocityObserver InitializeAngularVelocityObserver(ClockGenerator* clock_generator, const std::string file_name, double component_step_time_s,
+                                                          const Dynamics* dynamics);
+
+#endif  // S2E_COMPONENTS_IDEAL_ANGULAR_VELOCITY_OBSERVER_HPP_
