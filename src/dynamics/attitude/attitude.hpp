d243540a1acefde7df0b8b37357df6840dea7339
TomokiMochizuki <mochizuki@space.t.u-tokyo.ac.jp>
2024-06-07 10:22:58 +0900

    fix small

--- src/dynamics/attitude/attitude.hpp
+++ src/dynamics/attitude/attitude.hpp
@@ -119,7 +119,6 @@ class Attitude : public ILoggable, public SimulationObject {
   libra::Vector<3> angular_velocity_b_rad_s_;       //!< Angular velocity of spacecraft body fixed frame with respect to the inertial frame [rad/s]
   libra::Quaternion quaternion_i2b_;                //!< Attitude quaternion from the inertial frame to the body fixed frame
   libra::Vector<3> torque_b_Nm_;                    //!< Torque in the body fixed frame [Nm]
-
   const libra::Matrix<3, 3>& inertia_tensor_kgm2_;  //!< Inertia tensor of the spacecraft [kg m^2]
 
   libra::Vector<3> angular_momentum_spacecraft_b_Nms_;      //!< Angular momentum of spacecraft in the body fixed frame [Nms]
