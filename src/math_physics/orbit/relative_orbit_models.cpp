538e3b3a490861a059cf8115813a4ad59d6244cc
YuseiAlexS <ysueialexs@gmail.com>
2024-09-23 16:21:44 +0900

    stm + stm_inv

--- src/math_physics/orbit/relative_orbit_models.cpp
+++ src/math_physics/orbit/relative_orbit_models.cpp
@@ -117,13 +117,19 @@ math::Matrix<6, 6> CalcSsStm(double orbit_radius_m, double gravity_constant_m3_s
   return stm;
 }
 
+math::Matrix<6, 6> CalcSabatiniStm(double orbit_radius_m, double gravity_constant_m3_s2, double elapsed_time_s, OrbitalElements* reference_oe) {
+  math::Matrix<6, 6> stm;
+  // ここでstmを計算してください
+  return stm;
+}
+
 math::Vector<6> CalcSsCorrectionTerm(double orbit_radius_m, double gravity_constant_m3_s2, double elapsed_time_s, OrbitalElements* reference_oe) {
   math::Vector<6> correction_term;
   // ここでstmを計算してください
   return correction_term;
 }
 
-math::Matrix<6, 6> CalcSabatiniStm(double orbit_radius_m, double gravity_constant_m3_s2, double elapsed_time_s, OrbitalElements* reference_oe) {
+math::Matrix<6, 6> CalcCarterStm(double orbit_radius_m, double gravity_constant_m3_s2, double f_ref_rad, OrbitalElements* reference_oe){
   math::Matrix<6, 6> stm;
   // ここでstmを計算してください
   double n = sqrt(gravity_constant_m3_s2 / pow(orbit_radius_m, 3));
@@ -141,30 +147,30 @@ math::Matrix<6, 6> CalcSabatiniStm(double orbit_radius_m, double gravity_constan
   double term2_phi2_prime = 2 * e * sin(f_ref_rad) * k * (cos(f_ref_rad) / pow(k, 3)) - (cos(f_ref_rad) / pow(1 - pow(e, 2), 5.0 / 2.0));
   double phi2_prime = term1_phi2_prime + term2_phi2_prime;
   double phi3_prime = k - 4 * e * pow(sin(f_ref_rad), 3) / pow(E_rad / 2., 3) + 2 * cos(f_ref_rad) * sin(f_ref_rad) / pow(E_rad / 2., 2) - 6 * e * cos(f_ref_rad) * E_rad / 2. / pow(1 - pow(e, 2), 5.0 / 2.0);
-  double S_phi1 = -cos(f_ref_rad) - (e / 2.0) * pow(cos(f_ref_rad), 2);
-  double S_phi2 = 3 * e * k * k * K2 - (sin(f_ref_rad) / k);
-  double S_phi3 = -6 * k * k * K2 - ((2 + k) / (2 * k)) * sin(2 * f_ref_rad); 
-  stm[0][0] = 0.0;
-  stm[0][1] = 0.0;
-  stm[0][2] = 0.0;
+  double S1 = -cos(f_ref_rad) - (e / 2.0) * pow(cos(f_ref_rad), 2);
+  double S2 = 3 * e * k * k * K2 - (sin(f_ref_rad) / k);
+  double S3 = -6 * k * k * K2 - ((2 + k) / (2 * k)) * sin(2 * f_ref_rad); 
+  stm[0][0] = phi1;
+  stm[0][1] = phi2;
+  stm[0][2] = phi3;
   stm[0][3] = 0.0;
   stm[0][4] = 0.0;
   stm[0][5] = 0.0;
-  stm[1][0] = 0.0;
-  stm[1][1] = 0.0;
-  stm[1][2] = 0.0;
+  stm[1][0] = phi1_prime;
+  stm[1][1] = phi2_prime;
+  stm[1][2] = phi3_prime;
   stm[1][3] = 0.0;
   stm[1][4] = 0.0;
   stm[1][5] = 0.0;
-  stm[2][0] = 0.0;
-  stm[2][1] = 0.0;
-  stm[2][2] = 0.0;
-  stm[2][3] = 0.0;
+  stm[2][0] = -2*S1;
+  stm[2][1] = -2*S2;
+  stm[2][2] = -S3;
+  stm[2][3] = -1;
   stm[2][4] = 0.0;
   stm[2][5] = 0.0;
-  stm[3][0] = 0.0;
-  stm[3][1] = 0.0;
-  stm[3][2] = 0.0;
+  stm[3][0] = -2*phi1;
+  stm[3][1] = -2*phi2;
+  stm[3][2] = -(2*phi3 + 1);
   stm[3][3] = 0.0;
   stm[3][4] = 0.0;
   stm[3][5] = 0.0;
@@ -172,14 +178,52 @@ math::Matrix<6, 6> CalcSabatiniStm(double orbit_radius_m, double gravity_constan
   stm[4][1] = 0.0;
   stm[4][2] = 0.0;
   stm[4][3] = 0.0;
-  stm[4][4] = 0.0;
-  stm[4][5] = 0.0;
+  stm[4][4] = cos(f_ref_rad);
+  stm[4][5] = sin(f_ref_rad);
   stm[5][0] = 0.0;
   stm[5][1] = 0.0;
   stm[5][2] = 0.0;
   stm[5][3] = 0.0;
-  stm[5][4] = 0.0;
-  stm[5][5] = 0.0;
+  stm[5][4] = -sin(f_ref_rad);
+  stm[5][5] = cos(f_ref_rad);
+
+  stm_inv[0][0] = 4*S2 + phi2_prime;
+  stm_inv[0][1] = -phi2;
+  stm_inv[0][2] = 0.0;
+  stm_inv[0][3] = 2*S2;
+  stm_inv[0][4] = 0.0;
+  stm_inv[0][5] = 0.0;
+  stm_inv[1][0] = -(4*S1 + phi1_prime);
+  stm_inv[1][1] = phi1;
+  stm_inv[1][2] = 0.0;
+  stm_inv[1][3] = -2*S1;
+  stm_inv[1][4] = 0.0;
+  stm_inv[1][5] = 0.0;
+  stm_inv[2][0] = -2;
+  stm_inv[2][1] = 0.0;
+  stm_inv[2][2] = 0.0;
+  stm_inv[2][3] = -1;
+  stm_inv[2][4] = 0.0;
+  stm_inv[2][5] = 0.0;
+  stm_inv[3][0] = 2*S3 + phi3_prime;
+  stm_inv[3][1] = -phi3;
+  stm_inv[3][2] = -1;
+  stm_inv[3][3] = S3;
+  stm_inv[3][4] = 0.0;
+  stm_inv[3][5] = 0.0;
+  stm_inv[4][0] = 0.0;
+  stm_inv[4][1] = 0.0;
+  stm_inv[4][2] = 0.0;
+  stm_inv[4][3] = 0.0;
+  stm_inv[4][4] = cos(f_ref_rad);
+  stm_inv[4][5] = -sin(f_ref_rad);
+  stm_inv[5][0] = 0.0;
+  stm_inv[5][1] = 0.0;
+  stm_inv[5][2] = 0.0;
+  stm_inv[5][3] = 0.0;
+  stm_inv[5][4] = sin(f_ref_rad);
+  stm_inv[5][5] = cos(f_ref_rad);
+
   return stm;
 }
 
