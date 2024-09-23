628056f075b24541812927147bfe2f4a8ad7c117
TomokiMochizuki <mochizuki@space.t.u-tokyo.ac.jp>
2024-09-23 17:42:40 +0900

    add STM calculator

--- src/math_physics/orbit/relative_orbit_yamanaka_ankersen.hpp
+++ src/math_physics/orbit/relative_orbit_yamanaka_ankersen.hpp
@@ -31,22 +31,20 @@ class RelativeOrbitYamanakaAnkersen {
   /**
    * @fn CalculateInitialInverseMatrix
    * @brief Calculate position and velocity with Kepler orbit propagation
-   * @param [in] orbit_radius_m: Orbit radius [m]
-   * @param [in] gravity_constant_m3_s2: Gravity constant of the center body [m3/s2]
    * @param [in] f_ref_rad: True anomaly of the reference satellite [rad]
    * @param [in] reference_oe: Orbital elements of reference satellite
    */
-  void CalculateInitialInverseMatrix(double orbit_radius_m, double gravity_constant_m3_s2, double f_ref_rad, OrbitalElements* reference_oe);
+  void CalculateInitialInverseMatrix(double f_ref_rad, OrbitalElements* reference_oe);
 
   /**
    * @fn CalculateSTM
    * @brief Calculate position and velocity with Kepler orbit propagation
-   * @param [in] orbit_radius_m: Orbit radius [m]
    * @param [in] gravity_constant_m3_s2: Gravity constant of the center body [m3/s2]
+   * @param [in] elapsed_time_s: Elapsed time [s]
    * @param [in] f_ref_rad: True anomaly of the reference satellite [rad]
    * @param [in] reference_oe: Orbital elements of reference satellite
    */
-  math::Matrix<6, 6> CalculateSTM(double orbit_radius_m, double gravity_constant_m3_s2, double f_ref_rad, OrbitalElements* reference_oe);
+  math::Matrix<6, 6> CalculateSTM(double gravity_constant_m3_s2, double elapsed_time_s, double f_ref_rad, OrbitalElements* reference_oe);
 
   /**
    * @fn GetInitialInverseMatrix
