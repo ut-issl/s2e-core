cc33b28b57ed0df5b9f04b1c6215186fa5ef0549
kai0722 <nakamurakai0722@gmail.com>
2025-04-19 22:05:50 +0900

    Enhance logging in PowerControlUnit to include power port status, voltage, current, and power metrics

--- src/components/real/power/power_control_unit.cpp
+++ src/components/real/power/power_control_unit.cpp
@@ -46,12 +46,30 @@ int PowerControlUnit::ClosePort(const int port_id) {
 }
 
 std::string PowerControlUnit::GetLogHeader() const {
+
+  std::string prefix = "PCU_";
+
   std::string str_tmp = "";
-  return str_tmp;
+  for (const auto& [port_id, port] : power_ports_) {
+  str_tmp += logger::WriteScalar(prefix + "power_port_" + std::to_string(port_id) + "_switch_status");
+  str_tmp += logger::WriteScalar(prefix + "power_port_" + std::to_string(port_id) + "_voltage", "V");
+  str_tmp += logger::WriteScalar(prefix + "power_port_" + std::to_string(port_id) + "_current", "A");
+  str_tmp += logger::WriteScalar(prefix + "power_port_" + std::to_string(port_id) + "_power", "W");
+  }
+return str_tmp;
 }
 
 std::string PowerControlUnit::GetLogValue() const {
   std::string str_tmp = "";
+  for (const auto& [port_id, port] : power_ports_) {
+    str_tmp += logger::WriteScalar(port->GetIsOn());
+    double voltage = port->GetVoltage_V();
+    double current = port->GetCurrentConsumption_A();
+    double power = voltage * current;
+    str_tmp += logger::WriteScalar(voltage);
+    str_tmp += logger::WriteScalar(current);
+    str_tmp += logger::WriteScalar(power);
+  }
   return str_tmp;
 }
 
